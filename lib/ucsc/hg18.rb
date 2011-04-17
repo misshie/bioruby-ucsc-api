a09ebcb5fc569cc92e4e2029cffad8e6c6623cae
Hiroyuki Mishima <missy@be.to>
2011-04-17 11:50:41 +0900

    hg18.rb, hg19.rb, utils.rb are added

---
+++ lib/ucsc/hg18.rb
@@ -0,0 +1,16 @@
+#
+# = ucsc.rb
+# Copyright::   Cioyrught (C) 2011
+#               MISHIMA, Hiroyuki <missy at be.to / hmishima at nagasaki-u.ac.jp> 
+# Copyright::   Copyright (C) 2008
+#               Jan Aerts <jan.aerts@gmail.com>
+# License::     Ruby's
+
+module Bio
+  module Ucsc
+    require "hg18/db_connection.rb"
+    require "hg18/activerecord.rb"
+    require "hg18/slice.rb"
+    require "hg18/const.rb"
+  end
+end
---
+++ lib/ucsc/hg19.rb
@@ -0,0 +1,14 @@
+#
+# = hg19.rb
+# Copyright::   Cioyrught (C) 2011
+#               MISHIMA, Hiroyuki <missy at be.to / hmishima at nagasaki-u.ac.jp> 
+# License::     Ruby's
+
+module Bio
+  module Ucsc
+    require "hg19/db_connection.rb"
+    require "hg19/activerecord.rb"
+    require "hg19/slice.rb"
+    require "hg19/const.rb"
+  end
+end
