9ab85b63134e1381ae3f730bb7584e125719a3b4
Hiroyuki Mishima <missy@be.to>
2011-04-24 09:32:18 +0900

    hg19/trans.rb & comments in hg19.rb are added.

--- lib/bio-ucsc/hg19.rb
+++ lib/bio-ucsc/hg19.rb
@@ -13,24 +13,39 @@ module Bio
   module Ucsc
     module Hg19
       base = File.dirname(__FILE__)
-      autoload :Dgv,                  "#{base}/hg19/dgv"
 
+      # group: Mapping and Sequencing Tracks
+      ## track: Chromosome Band
+      autoload :CytoBand,             "#{base}/hg19/cytoband"
+
+      # group: Genes and Gene Prediction Tracks
+      ## track: UCSC Genes
+      autoload :KnownGene,            "#{base}/hg19/knowngene"
+
+      ## track: RefSeq Genes
+      autoload :RefGene,              "#{base}/hg19/refgene"      
+
+      ## track: Ensemble Genes
+      autoload :EnsGene,              "#{base}/hg19/ensgene"
+
+      ## track: sno/miRNA
+      autoload :WgRna,                "#{base}/hg19/wgrna"
+
+      ## track: tRNA Genes             
+      autoload :TRNAs,                "#{base}/hg19/trnas"
+
+      # group: Variation and Repeats
+      ## track: SNPs(131)
       autoload :Snp131,               "#{base}/hg19/snp131"
 
+      ## tracks: All/Common/Flagged/Mult SNPs(132)
       autoload :Snp132,               "#{base}/hg19/snp132"
       autoload :Snp132Common,         "#{base}/hg19/snp132common"
       autoload :Snp132Flagged,        "#{base}/hg19/snp132flagged"
       autoload :Snp132Mult,           "#{base}/hg19/snp132mult"
       autoload :Snp132CodingDbSnp,    "#{base}/hg19/snp132codingdbsnp"
 
-      autoload :KnownGene,            "#{base}/hg19/knowngene"
-      autoload :RefGene,              "#{base}/hg19/refgene"      
-      autoload :GwasCatalog,          "#{base}/hg19/gwascatalog"
-      autoload :CytoBand,             "#{base}/hg19/cytoband"
-      autoload :OmimGene,             "#{base}/hg19/omimgene"
-      autoload :WgRna,                "#{base}/hg19/wgrna"
-      autoload :EnsGene,              "#{base}/hg19/ensgene"
-
+      ## track: HapMap SNPs
       autoload :HapMapSnpsASW,        "#{base}/hg19/hapmapsnpsasw"
       autoload :HapMapSnpsCEU,        "#{base}/hg19/hapmapsnpsceu"
       autoload :HapMapSnpsCHB,        "#{base}/hg19/hapmapsnpschb"
@@ -45,11 +60,25 @@ module Bio
       autoload :HapMapAllelesChimp,   "#{base}/hg19/hapmapalleleschimp"
       autoload :HapMapAllelesMacaque, "#{base}/hg19/hapmapallelesmacaque"
 
+      ## track: RepeatMasker
       autoload :Rmsk,                 "#{base}/hg19/rmsk"
 
+      ## track: DGV Struct Var
+      autoload :Dgv,                  "#{base}/hg19/dgv"
+
+      # group: Comparative Genomics
+      ## track: Conservation
       autoload :PhyloP46wayPrimates,  "#{base}/hg19/phylop46wayprimates"
       autoload :PhastConsElements46wayPrimates, "#{base}/hg19/phastconselements46wayprimates"
 
+      # group: Phenotype and Desease Association
+      ## track: OMIM Genes
+      autoload :OmimGene,             "#{base}/hg19/omimgene"
+
+      ## track: :Gwas Catalog
+      autoload :GwasCatalog,          "#{base}/hg19/gwascatalog"
+
+      # information tables behind tracks
       autoload :Description,          "#{base}/hg19/description"
       autoload :GbCdnaInfo,           "#{base}/hg19/gbcdnainfo"
       autoload :KgXref,               "#{base}/hg19/kgxref"
