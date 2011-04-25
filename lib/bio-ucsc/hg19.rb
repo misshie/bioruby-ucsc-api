5769428b6acd6fd3568f4386a209e6d776372831
Hiroyuki Mishima <missy@be.to>
2011-04-26 08:21:29 +0900

    added: hg19/pgna12892.rb

--- lib/bio-ucsc/hg19.rb
+++ lib/bio-ucsc/hg19.rb
@@ -101,7 +101,7 @@ module Bio
       ## track: Genome Variants (Personal Genomes)
       autoload :PgNA12878,            "#{base}/pgna12878" # CEU daught
       autoload :PgNA12891,            "#{base}/pgna12891" # CEU father
-      
+      autoload :PgNA12892,            "#{base}/pgna12892" # CEU mother
 
 
       # group: Comparative Genomics
