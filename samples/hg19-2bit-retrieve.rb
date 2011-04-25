b923d89fe8c2ffbd457814b982ec70e7f27d5c50
Hiroyuki Mishima <missy@be.to>
2011-04-25 15:18:54 +0900

    sample/hg19-2bit-retrieve.rb is added. README is updated.

---
+++ samples/hg19-2bit-retrieve.rb
@@ -0,0 +1,39 @@
+#!/usr/local/bin/ruby-1.9
+#
+# Usage:: hg19-2bit-retrieve.rb <genomic interval>
+#         (default is "chr1:9,500-10,499")
+#
+# Copyright::
+#   Copyright (C) 2011 MISHIMA, Hiroyuki
+#                      <missy at be.to / hmishima at ngit agasaki-u.ac.jp>
+# License::     The Ruby licence (Ryby's / GPLv2 dual)
+#
+
+require 'nkf'
+
+HG19_2BIT_FILE = "hg19.2bit"
+
+require File.dirname(__FILE__) + '/../lib/bio-ucsc'
+
+class Hg19Ref
+  include Bio::Ucsc::Hg19
+
+  def run(interval)
+    DBConnection.connect
+    Reference.load(HG19_2BIT_FILE)
+    itv = Bio::GenomicInterval.parse(interval)
+
+    puts itv.to_s
+    puts NKF.nkf("-wf50-0", Reference.find_by_interval(itv))
+  end
+end
+
+if $0 == __FILE__
+  interval = ARGV[0]
+  interval ||= "chr1:9,500-10,999"
+  Hg19Ref.new.run(interval)
+end
+
+
+
+
