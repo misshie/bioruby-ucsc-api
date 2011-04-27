cbfe5dc9d9f126a4a8f9b3988d8d0b859a5e7049
Hiroyuki Mishima <missy@be.to>
2011-04-28 00:00:18 +0900

    modified:   spec/hg18/refgene_spec.rb

--- spec/hg18/refgene_spec.rb
+++ spec/hg18/refgene_spec.rb
@@ -7,7 +7,7 @@ describe "Bio::Ucsc::Hg18::RefGene" do
         Bio::Ucsc::Hg18::DBConnection.default
         Bio::Ucsc::Hg18::DBConnection.connect
         i = Bio::GenomicInterval.parse("chr1:1-100,000")
-        Bio::Ucsc::Hg18::RefGene.find_by_interval(i).should have(5).items
+        Bio::Ucsc::Hg18::RefGene.find_all_by_interval(i).should have(5).items
       end
 
       it "returens an array of results with column accessors" do
@@ -15,7 +15,7 @@ describe "Bio::Ucsc::Hg18::RefGene" do
         Bio::Ucsc::Hg18::DBConnection.connect
         i = Bio::GenomicInterval.parse("chr1:1-100,000")
         r = Bio::Ucsc::Hg18::RefGene.find_by_interval(i)
-        r[0].chrom.should == "chr1"
+        r.chrom.should == "chr1"
       end
     end
   end
