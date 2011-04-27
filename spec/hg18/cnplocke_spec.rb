d6f3814f13631bf8d2484576215899453da0e7f0
Hiroyuki Mishima <missy@be.to>
2011-04-27 23:54:29 +0900

    modified:   spec/hg18/cnplocke_spec.rb

--- spec/hg18/cnplocke_spec.rb
+++ spec/hg18/cnplocke_spec.rb
@@ -7,7 +7,7 @@ describe "Bio::Ucsc::Hg18::CnpLocke" do
         Bio::Ucsc::Hg18::DBConnection.default
         Bio::Ucsc::Hg18::DBConnection.connect
         i = Bio::GenomicInterval.parse("chr1:1-2,000,000")
-        Bio::Ucsc::Hg18::CnpLocke.find_by_interval(i).should have(2).items
+        Bio::Ucsc::Hg18::CnpLocke.find_all_by_interval(i).should have(2).items
       end
 
       it "returens an array of results with column accessors" do
@@ -15,7 +15,7 @@ describe "Bio::Ucsc::Hg18::CnpLocke" do
         Bio::Ucsc::Hg18::DBConnection.connect
         i = Bio::GenomicInterval.parse("chr1:1-2,000,000")
         r = Bio::Ucsc::Hg18::CnpLocke.find_by_interval(i)
-        r[0].chrom.should == "chr1"
+        r.chrom.should == "chr1"
       end
     end
   end
