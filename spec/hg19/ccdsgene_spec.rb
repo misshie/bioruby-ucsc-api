5a34c0a86b44a049d3eac2cda619b91d8e0f9315
Hiroyuki Mishima <missy@be.to>
2011-04-28 09:35:17 +0900

    modified: spec/hg19/ccdsgene_spec.rb

--- spec/hg19/ccdsgene_spec.rb
+++ spec/hg19/ccdsgene_spec.rb
@@ -7,7 +7,7 @@ describe "Bio::Ucsc::Hg19::CcdsGene" do
         Bio::Ucsc::Hg19::DBConnection.default
         Bio::Ucsc::Hg19::DBConnection.connect
         i = Bio::GenomicInterval.parse("chr1:1-1,000,000")
-        Bio::Ucsc::Hg19::CcdsGene.find_by_interval(i).should have(11).items
+        Bio::Ucsc::Hg19::CcdsGene.find_all_by_interval(i).should have(11).items
       end
 
       it "returens an array of results with column accessors" do
@@ -15,7 +15,7 @@ describe "Bio::Ucsc::Hg19::CcdsGene" do
         Bio::Ucsc::Hg19::DBConnection.connect
         i = Bio::GenomicInterval.parse("chr1:1-1,000,000")
         r = Bio::Ucsc::Hg19::CcdsGene.find_by_interval(i)
-        r[0].chrom.should == "chr1"
+        r.chrom.should == "chr1"
       end
     end
   end
