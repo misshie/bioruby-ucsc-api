6467b354b0279cd3e76b0650e9b15a3eee58c73c
Hiroyuki Mishima <missy@be.to>
2011-04-28 13:11:15 +0900

    added: wgencodegencodeautov4.rb

--- lib/bio-ucsc/hg19.rb
+++ lib/bio-ucsc/hg19.rb
@@ -86,6 +86,7 @@ module Bio
       autoload :KnownAlt,             "#{base}/knownalt"
       ## track: Gencode Genes
       autoload :WgEncodeGencodeManualV4, "#{base}/wgencodegencodemanualv4"
+      autoload :WgEncodeGencodeAutoV4, "#{base}/wgencodegencodeautov4"
       ## track: RefSeq Genes
       autoload :RefGene,              "#{base}/refgene"      
       ## track: Ensemble Genes
---
+++ spec/hg19/wgencodegencodeautov4_spec.rb
@@ -0,0 +1,24 @@
+require 'bio-ucsc'
+describe "Bio::Ucsc::Hg19::WgEncodeGencodeAutoV4" do 
+
+  describe "#find_by_id" do
+    context 'given interval "chr1:1-50,000"' do
+      it 'returns 7 items"' do
+        Bio::Ucsc::Hg19::DBConnection.default
+        Bio::Ucsc::Hg19::DBConnection.connect
+        i = Bio::GenomicInterval.parse("chr1:1-50,000")
+        r = Bio::Ucsc::Hg19::WgEncodeGencodeAutoV4.find_all_by_interval(i)
+        r.should have(4).items
+      end
+
+      it 'returns (r.chrom === "chr1")' do
+        Bio::Ucsc::Hg19::DBConnection.default
+        Bio::Ucsc::Hg19::DBConnection.connect
+        i = Bio::GenomicInterval.parse("chr1:1-50,000")
+        r = Bio::Ucsc::Hg19::WgEncodeGencodeAutoV4.find_by_interval(i)
+        r.chrom.should == "chr1"
+      end
+    end
+  end
+
+end
