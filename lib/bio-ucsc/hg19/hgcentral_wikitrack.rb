34ba12f2c7988006b13cbbc6aab38c1f6395e49e
Hiroyuki Mishima <missy@be.to>
2011-04-30 22:16:41 +0900

    modified: hg19/hgcentral_wikitrack_spec.rb wikitrack is not in the hg19 table but the hgcentral table

--- lib/bio-ucsc/hg19/hgcentral_wikitrack.rb
+++ lib/bio-ucsc/hg19/hgcentral_wikitrack.rb
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
-# = hg19/wikitrack.rb
+# = hg19/hgcentral_wikitrack.rb
 # Copyright::
 #  Copyright (C) 2011 MISHIMA, Hiroyuki <missy at be.to / hmishima at nagasaki-u.ac.jp> 
 # License::     The Ruby licence (Ryby's / GPLv2 dual)
 #
-# = Table desfription in UCSC Table Browser 
+# = Table desfription in UCSC Table Browser
 # The Wiki track displays genome annotations added by the Genome
 # Browser user community. These annotations have not been curated.
+#
+# Database: hgcentral    Primary Table: wikiTrack
 #
 # = ommitted dynamic method(s) due to the method name collision
 #  none
@@ -14,10 +16,11 @@
 module Bio
   module Ucsc
     module Hg19
-      class WikiTrack < DBConnection
+      class Hgcentral_WikiTrack < DBConnection
         extend Bio::Ucsc::Hg19::QueryUsingChromBin
-        set_table_name 'wikiTrack'
+        set_table_name 'hgcentral.wikiTrack'
         set_primary_key nil
+        columns_hash.delete("class")
       end 
     end # module Hg19
   end # module Ucsc
