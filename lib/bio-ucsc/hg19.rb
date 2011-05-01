f9180e13cc3917e17b3c167b89a6cc370bbe90a3
Hiroyuki Mishima <missy@be.to>
2011-05-01 14:57:06 +0900

    added: hg19/wgencodeaffyrnachipfilttransfragshepg2nucleuslongnonpolya.rb

--- lib/bio-ucsc/hg19.rb
+++ lib/bio-ucsc/hg19.rb
@@ -277,7 +277,7 @@ module Bio
       autoload :WgEncodeAffyRnaChipFiltTransfragsGm12878NucleolusTotal, "#{base}/wgencodeaffyrnachipfilttransfragsgm12878nucleolustotal"
       autoload :WgEncodeAffyRnaChipFiltTransfragsHepg2CytosolLongnonpolya, "#{base}/wgencodeaffyrnachipfilttransfragshepg2cytosollongnonpolya"
       autoload :WgEncodeAffyRnaChipFiltTransfragsHepg2CytosolLongpolya, "#{base}/wgencodeaffyrnachipfilttransfragshepg2cytosollongpolya"
-
+      autoload :WgEncodeAffyRnaChipFiltTransfragsHepg2NucleusLongnonpolya, "#{base}/wgencodeaffyrnachipfilttransfragshepg2nucleuslongnonpolya"
       # group: Regulation ----------
       #
 
