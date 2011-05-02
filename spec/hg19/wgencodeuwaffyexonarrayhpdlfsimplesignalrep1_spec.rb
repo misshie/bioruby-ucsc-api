de9974fe313424883e8ef0494413987c69a5c0d0
Hiroyuki Mishima <missy@be.to>
2011-05-02 12:53:45 +0900

    All tables in the Expression group are implemented. Some "pointer only" tables are excepted. All tables in the UW Affy Exon track are added.

---
+++ spec/hg19/wgencodeuwaffyexonarrayhpdlfsimplesignalrep1_spec.rb
@@ -0,0 +1,24 @@
+require 'bio-ucsc'
+describe "Bio::Ucsc::Hg19::WgEncodeUwAffyExonArrayHpdlfSimpleSignalRep1" do 
+
+  describe "#find_by_interval" do
+    context "given range chr1:1-20,000" do
+      it "returns an array of records" do
+        Bio::Ucsc::Hg19::DBConnection.default
+        Bio::Ucsc::Hg19::DBConnection.connect
+        i = Bio::GenomicInterval.parse("chr1:1-20,000")
+        r = Bio::Ucsc::Hg19::WgEncodeUwAffyExonArrayHpdlfSimpleSignalRep1.find_all_by_interval(i)
+        r.should have(41).items
+      end
+
+      it 'returns a record (r.chrom == "chr1")' do
+        Bio::Ucsc::Hg19::DBConnection.default
+        Bio::Ucsc::Hg19::DBConnection.connect
+        i = Bio::GenomicInterval.parse("chr1:1-20,000")
+        r = Bio::Ucsc::Hg19::WgEncodeUwAffyExonArrayHpdlfSimpleSignalRep1.find_by_interval(i)
+        r.chrom.should == "chr1"
+      end
+    end
+  end
+
+end
