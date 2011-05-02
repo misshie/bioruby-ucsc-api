07824c6e20c3709e52f3b2f66001f44d17b997a5
Hiroyuki Mishima <missy@be.to>
2011-05-02 21:20:01 +0900

    All the "Regulation-UW DNaseI HS" tables are implemented.

---
+++ spec/hg19/wgencodeuwdnasehmvecdlyadpkrep2_spec.rb
@@ -0,0 +1,24 @@
+require 'bio-ucsc'
+describe "Bio::Ucsc::Hg19::WgEncodeUwDnaseHmvecdlyadPkRep2" do 
+
+  describe "#find_by_interval" do
+    context "given range chr1:1-800,000" do
+      it "returns an array of records" do
+        Bio::Ucsc::Hg19::DBConnection.default
+        Bio::Ucsc::Hg19::DBConnection.connect
+        i = Bio::GenomicInterval.parse("chr1:1-800,000")
+        r = Bio::Ucsc::Hg19::WgEncodeUwDnaseHmvecdlyadPkRep2.find_all_by_interval(i)
+        r.should have(23).items
+      end
+
+      it 'returns a record (r.chrom == "chr1")' do
+        Bio::Ucsc::Hg19::DBConnection.default
+        Bio::Ucsc::Hg19::DBConnection.connect
+        i = Bio::GenomicInterval.parse("chr1:1-800,000")
+        r = Bio::Ucsc::Hg19::WgEncodeUwDnaseHmvecdlyadPkRep2.find_by_interval(i)
+        r.chrom.should == "chr1"
+      end
+    end
+  end
+
+end
