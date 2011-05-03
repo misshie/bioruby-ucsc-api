2517891f51c4ec7fe32058ffc4e3acbd68386037
Hiroyuki Mishima <missy@be.to>
2011-05-03 21:23:43 +0900

    All "Regulation-SUBY RUP Tiling" tables are added

---
+++ devtool/SUNYRIPTiling/autoload_snippet.rb
@@ -0,0 +1,9 @@
+      ## track: SUBY RIP Tiling
+      autoload :WgEncodeSunyAlbanyTilingGm12878Elavl1RbpAssocRna, "#{base}/wgencodesunyalbanytilinggm12878elavl1rbpassocrna"
+      autoload :WgEncodeSunyAlbanyTilingGm12878Pabpc1RbpAssocRna, "#{base}/wgencodesunyalbanytilinggm12878pabpc1rbpassocrna"
+      autoload :WgEncodeSunyAlbanyTilingGm12878T7tagRbpAssocRna, "#{base}/wgencodesunyalbanytilinggm12878t7tagrbpassocrna"
+      autoload :WgEncodeSunyAlbanyTilingGm12878RipinputRbpAssocRna, "#{base}/wgencodesunyalbanytilinggm12878ripinputrbpassocrna"
+      autoload :WgEncodeSunyAlbanyTilingK562Elavl1RbpAssocRna, "#{base}/wgencodesunyalbanytilingk562elavl1rbpassocrna"
+      autoload :WgEncodeSunyAlbanyTilingK562Pabpc1RbpAssocRna, "#{base}/wgencodesunyalbanytilingk562pabpc1rbpassocrna"
+      autoload :WgEncodeSunyAlbanyTilingK562T7tagRbpAssocRna, "#{base}/wgencodesunyalbanytilingk562t7tagrbpassocrna"
+      autoload :WgEncodeSunyAlbanyTilingK562RipinputRbpAssocRna, "#{base}/wgencodesunyalbanytilingk562ripinputrbpassocrna"
