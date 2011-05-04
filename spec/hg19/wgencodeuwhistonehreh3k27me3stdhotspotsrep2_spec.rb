7d3537d8dcee180c762ab2368daaf1c1f041e159
Hiroyuki Mishima <missy@be.to>
2011-05-04 16:14:06 +0900

    All 'Regulation-UW Histone' tables except filename-only tables are added

---
+++ spec/hg19/wgencodeuwhistonehreh3k27me3stdhotspotsrep2_spec.rb
@@ -0,0 +1,16 @@
+require 'bio-ucsc'
+describe "Bio::Ucsc::Hg19::WgEncodeUwHistoneHreH3k27me3StdHotspotsRep2" do 
+
+  describe "#find_by_interval" do
+    context "given range chr1:1-800,000" do
+      it 'returns a record (r.chrom == "chr1")' do
+        Bio::Ucsc::Hg19::DBConnection.default
+        Bio::Ucsc::Hg19::DBConnection.connect
+        i = Bio::GenomicInterval.parse("chr1:1-800,000")
+        r = Bio::Ucsc::Hg19::WgEncodeUwHistoneHreH3k27me3StdHotspotsRep2.find_by_interval(i)
+        r.chrom.should == "chr1"
+      end
+    end
+  end
+
+end
