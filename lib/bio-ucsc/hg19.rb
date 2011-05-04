eca788b1feae7b23a28cb9875f37a725a60e5db5
Hiroyuki Mishima <missy@be.to>
2011-05-05 08:55:35 +0900

    added: hg19/netorycun2.rb

--- lib/bio-ucsc/hg19.rb
+++ lib/bio-ucsc/hg19.rb
@@ -1616,7 +1616,7 @@ module Bio
       autoload :ChainCavPor3,         "#{base}/chaincavpor3"
       autoload :NetCavPor3,           "#{base}/netcavpor3"
       autoload :ChainOryCun2,         "#{base}/chainorycun2"
-
+      autoload :NetOryCun2,           "#{base}/netorycun2"
 
 
 
