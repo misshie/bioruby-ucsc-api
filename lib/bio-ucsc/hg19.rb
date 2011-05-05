ec2b62ae5d0a7463872b1a75c777face64b0669e
Hiroyuki Mishima <missy@be.to>
2011-05-05 09:58:14 +0900

    added: hg19/netcanfam2.rb

--- lib/bio-ucsc/hg19.rb
+++ lib/bio-ucsc/hg19.rb
@@ -1626,6 +1626,7 @@ module Bio
       autoload :ChainFelCat4,         "#{base}/chainfelcat4"
       autoload :NetFelCat4,           "#{base}/netfelcat4"
       autoload :ChainCanFam2,         "#{base}/chaincanfam2"
+      autoload :NetCanFam2,           "#{base}/netcanfam2"
 
 
 
