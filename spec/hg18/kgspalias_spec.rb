bc1a9f6b2759010a92507f8c58e4407bd444934e
Hiroyuki Mishima <missy@be.to>
2011-05-23 22:12:59 +0900

    added: hg18/kgspalias.rb

---
+++ spec/hg18/kgspalias_spec.rb
@@ -0,0 +1,15 @@
+require 'bio-ucsc'
+describe "Bio::Ucsc::Hg18::KgSpAlias" do 
+
+  describe "#find_by_kgID" do
+    context 'given "uc001aaa.2"' do
+      it 'returns an results (r.alias == "BC032353")' do
+        Bio::Ucsc::Hg18::DBConnection.default
+        Bio::Ucsc::Hg18::DBConnection.connect
+        r = Bio::Ucsc::Hg18::KgSpAlias.find_by_kgID("uc001aaa.2")
+        r.alias.should == "BC032353"
+      end
+    end
+  end
+
+end
