cd8439ddc64c1fca32cd3065fe6b014234b3d372
Hiroyuki Mishima <missy@be.to>
2011-06-03 17:12:51 +0900

    added: 138 tables hg18-Expression-Caltech RNA-seq

---
+++ lib/bio-ucsc/hg18/wgencodecaltechrnaseqminusrawsignalrep2gm12878cellpapbb2r1x75d.rb
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# = hg18/wgencodecaltechrnaseqminusrawsignalrep2gm12878cellpapbb2r1x75d.rb
+# Copyright::
+#  Copyright (C) 2011 MISHIMA, Hiroyuki
+#                <missy at be.to / hmishima at nagasaki-u.ac.jp> 
+# License::     The Ruby licence (Ryby's / GPLv2 dual)
+#
+# = Table desfription in UCSC Table Browser
+# This track is produced as part of the ENCODE Project.
+#
+#  none
+
+module Bio
+  module Ucsc
+    module Hg18
+      class WgEncodeCaltechRnaSeqMinusRawSignalRep2Gm12878CellPapBb2R1x75d < DBConnection
+        extend Bio::Ucsc::Hg18::QueryUsingChromBin
+        set_table_name 'wgEncodeCaltechRnaSeqMinusRawSignalRep2Gm12878CellPapBb2R1x75d'
+        set_primary_key nil
+      end 
+    end # module Hg18
+  end # module Ucsc
+end # Bio
---
+++ spec/hg18/wgencodecaltechrnaseqsplicesrep2hepg2cellpapbb2r2x75_spec.rb
@@ -0,0 +1,16 @@
+require 'bio-ucsc'
+describe "Bio::Ucsc::Hg18::WgEncodeCaltechRnaSeqSplicesRep2Hepg2CellPapBb2R2x75" do 
+
+  describe "#find_by_interval" do
+    context "given range chr1:1-5,000" do
+      it 'returns a record (r.chrom == "chr1")' do
+        Bio::Ucsc::Hg18::DBConnection.default
+        Bio::Ucsc::Hg18::DBConnection.connect
+        i = Bio::GenomicInterval.parse("chr1:1-5,000")
+        r = Bio::Ucsc::Hg18::WgEncodeCaltechRnaSeqSplicesRep2Hepg2CellPapBb2R2x75.find_by_interval(i)
+        r.chrom.should == "chr1"
+      end
+    end
+  end
+
+end
