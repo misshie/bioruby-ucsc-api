6ee916d53a75a76f2ed3a18f9c2ebe17f31862eb
Hiroyuki Mishima <missy@be.to>
2011-06-07 10:03:40 +0900

    added: 6 tables in hg18-Expression-HudsonAlpha RNA-seq

--- lib/bio-ucsc/hg18.rb
+++ lib/bio-ucsc/hg18.rb
@@ -953,6 +953,13 @@ module Bio
       ## track: Helicos RNA-seq
       autoload :WgEncodeHelicosRnaSeqRawSignalK562CytosolLongpolya, "#{base}/wgencodehelicosrnaseqrawsignalk562cytosollongpolya"
       autoload :WgEncodeHelicosRnaSeqAlignmentsK562CytosolLongpolya, "#{base}/wgencodehelicosrnaseqalignmentsk562cytosollongpolya"
+      ## track: HudsonAlpha RNA-seq
+      autoload :WgEncodeHudsonalphaRnaSeqRPKMRep1A549CellPapErng3R1x36Dexa, "#{base}/wgencodehudsonalpharnaseqrpkmrep1a549cellpaperng3r1x36dexa"
+      autoload :WgEncodeHudsonalphaRnaSeqRPKMRep1A549CellPapErng3R1x36Etoh02, "#{base}/wgencodehudsonalpharnaseqrpkmrep1a549cellpaperng3r1x36etoh02"
+      autoload :WgEncodeHudsonalphaRnaSeqRPKMRep2A549CellPapErng3R1x36Dexa, "#{base}/wgencodehudsonalpharnaseqrpkmrep2a549cellpaperng3r1x36dexa"
+      autoload :WgEncodeHudsonalphaRnaSeqRPKMRep2A549CellPapErng3R1x36Etoh02, "#{base}/wgencodehudsonalpharnaseqrpkmrep2a549cellpaperng3r1x36etoh02"
+      autoload :WgEncodeHudsonalphaRnaSeqRPKMRep1JurkatCellPapBow10R1x25, "#{base}/wgencodehudsonalpharnaseqrpkmrep1jurkatcellpapbow10r1x25"
+      autoload :WgEncodeHudsonalphaRnaSeqRPKMRep2JurkatCellPapBow10R1x25, "#{base}/wgencodehudsonalpharnaseqrpkmrep2jurkatcellpapbow10r1x25"
 
 
 
