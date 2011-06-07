14ee84241c72d08e26e5488b5af25f27a26e255e
Hiroyuki Mishima <missy@be.to>
2011-06-07 13:01:59 +0900

    added: 67 tables in hg18-Expression-UW Affy Exon

---
+++ lib/bio-ucsc/hg18/wgencodeuwaffyexonarraysimplesignalrep2caco2.rb
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# = hg18/wgencodeuwaffyexonarraysimplesignalrep2caco2.rb
+# Copyright::
+#  Copyright (C) 2011 MISHIMA, Hiroyuki
+#                <missy at be.to / hmishima at nagasaki-u.ac.jp> 
+# License::     The Ruby licence (Ryby's / GPLv2 dual)
+#
+# = Table desfription in UCSC Table Browser
+# This track displays human tissue microarray data using Affymetrix
+# Human Exon 1.0 GeneChip. The track was produced as part of the
+# ENCODE Project.
+#
+# = ommitted dynamic method(s) due to the method name collision
+#  none
+
+module Bio
+  module Ucsc
+    module Hg18
+      class WgEncodeUwAffyExonArraySimpleSignalRep2Caco2 < DBConnection
+        extend Bio::Ucsc::Hg18::QueryUsingChromBin
+        set_table_name 'wgEncodeUwAffyExonArraySimpleSignalRep2Caco2'
+        set_primary_key nil
+      end 
+    end # module Hg18
+  end # module Ucsc
+end # Bio
---
+++ spec/hg18/wgencodeuwaffyexonarraysimplesignalrep1gm06990_spec.rb
@@ -0,0 +1,16 @@
+require 'bio-ucsc'
+describe "Bio::Ucsc::Hg18::WgEncodeUwAffyExonArraySimpleSignalRep1Gm06990" do 
+
+  describe "#find_by_interval" do
+    context "given range chr1:1-10,000" do
+      it 'returns a record (r.chrom == "chr1")' do
+        Bio::Ucsc::Hg18::DBConnection.default
+        Bio::Ucsc::Hg18::DBConnection.connect
+        i = Bio::GenomicInterval.parse("chr1:1-10,000")
+        r = Bio::Ucsc::Hg18::WgEncodeUwAffyExonArraySimpleSignalRep1Gm06990.find_by_interval(i)
+        r.chrom.should == "chr1"
+      end
+    end
+  end
+
+end
