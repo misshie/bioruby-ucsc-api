91b8b717dff35418614b5d6d11917d43d22a89ab
Hiroyuki Mishima <missy@be.to>
2011-06-08 09:26:53 +0900

    8 tables in hg18-Regulation-Enhanced H3K27Ac

---
+++ devtool/hg18/EnhancedH3K27Ac/autoload_snippet.rb
@@ -0,0 +1,9 @@
+      ## track: Enhanced H3K27Ac
+      autoload :WgEncodeBroadChipSeqSignalGm12878H3k27ac, "#{base}/wgencodebroadchipseqsignalgm12878h3k27ac"
+      autoload :WgEncodeBroadChipSeqSignalHepg2H3k27ac, "#{base}/wgencodebroadchipseqsignalhepg2h3k27ac"
+      autoload :WgEncodeBroadChipSeqSignalHmecH3k27ac, "#{base}/wgencodebroadchipseqsignalhmech3k27ac"
+      autoload :WgEncodeBroadChipSeqSignalHsmmH3k27ac, "#{base}/wgencodebroadchipseqsignalhsmmh3k27ac"
+      autoload :WgEncodeBroadChipSeqSignalHuvecH3k27ac, "#{base}/wgencodebroadchipseqsignalhuvech3k27ac"
+      autoload :WgEncodeBroadChipSeqSignalK562H3k27ac, "#{base}/wgencodebroadchipseqsignalk562h3k27ac"
+      autoload :WgEncodeBroadChipSeqSignalNhekH3k27ac, "#{base}/wgencodebroadchipseqsignalnhekh3k27ac"
+      autoload :WgEncodeBroadChipSeqSignalNhlfH3k27ac, "#{base}/wgencodebroadchipseqsignalnhlfh3k27ac"
---
+++ lib/bio-ucsc/hg18/wgencodebroadchipseqsignalhepg2h3k27ac.rb
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# = hg18/wgencodebroadchipseqsignalhepg2h3k27ac.rb
+# Copyright::
+#  Copyright (C) 2011 MISHIMA, Hiroyuki
+#                <missy at be.to / hmishima at nagasaki-u.ac.jp> 
+# License::     The Ruby licence (Ryby's / GPLv2 dual)
+#
+# = Table desfription in UCSC Table Browser
+# Enhanced H3K27Ac - ENCODE Enhancer- and Promoter-Associated Histone
+# Mark (H3K27Ac) on 8 Cell Lines
+#
+# = ommitted dynamic method(s) due to the method name collision
+#  none
+
+module Bio
+  module Ucsc
+    module Hg18
+      class WgEncodeBroadChipSeqSignalHepg2H3k27ac < DBConnection
+        extend Bio::Ucsc::Hg18::QueryUsingChromBin
+        set_table_name 'wgEncodeBroadChipSeqSignalHepg2H3k27ac'
+        set_primary_key nil
+      end 
+    end # module Hg18
+  end # module Ucsc
+end # Bio
