e74f8651b6ff94442896fe6eefe1d621bf670d09
Hiroyuki Mishima <missy@be.to>
2011-06-08 22:07:51 +0900

    added: 6 tables in hg18-Regulation-GIS ChIP-PET

--- lib/bio-ucsc/hg18.rb
+++ lib/bio-ucsc/hg18.rb
@@ -1334,6 +1334,13 @@ module Bio
       autoload :Eponine , "#{base}/eponine"
       ## track: FirstEF
       autoload :FirstEF, "#{base}/firstef"
+      ## track: GIS ChIP-PET
+      autoload :WgEncodeGisChipPet, "#{base}/wgencodegischippet"
+      autoload :WgEncodeGisChipPetStat1Gif, "#{base}/wgencodegischippetstat1gif"
+      autoload :WgEncodeGisChipPetStat1NoGif, "#{base}/wgencodegischippetstat1nogif"
+      autoload :WgEncodeGisChipPetMycP493, "#{base}/wgencodegischippetmycp493"
+      autoload :WgEncodeGisChipPetHes3H3K4me3, "#{base}/wgencodegischippethes3h3k4me3"
+      autoload :WgEncodeGisChipPetHes3H3K27me3, "#{base}/wgencodegischippethes3h3k27me3"
 
 
 
