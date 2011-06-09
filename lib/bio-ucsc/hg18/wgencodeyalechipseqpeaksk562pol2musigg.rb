bfdc0a0c8c0b172b1f539a3d078d5951404f6a99
Hiroyuki Mishima <missy@be.to>
2011-06-09 15:56:35 +0900

    added: 128 tables in hg19/Regulation/Yale TFBS

---
+++ lib/bio-ucsc/hg18/wgencodeyalechipseqpeaksk562pol2musigg.rb
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# = hg18/wgencodeyalechipseqpeaksk562pol2musigg.rb
+# Copyright::
+#  Copyright (C) 2011 MISHIMA, Hiroyuki
+#                <missy at be.to / hmishima at nagasaki-u.ac.jp> 
+# License::     The Ruby licence (Ryby's / GPLv2 dual)
+#
+# = Table desfription in UCSC Table Browser
+# Yale TFBS - ENCODE Transcription Factor Binding Sites by ChIP-seq
+# from Yale/UC-Davis/Harvard
+#
+# = ommitted dynamic method(s) due to the method name collision
+#  none
+
+module Bio
+  module Ucsc
+    module Hg18
+      class WgEncodeYaleChIPseqPeaksK562Pol2Musigg < DBConnection
+        extend Bio::Ucsc::Hg18::QueryUsingChromBin
+        set_table_name 'wgEncodeYaleChIPseqPeaksK562Pol2Musigg'
+        set_primary_key nil
+      end 
+    end # module Hg18
+  end # module Ucsc
+end # Bio
