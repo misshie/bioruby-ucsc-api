143f1076acc1f082326ad451197e759446fef058
Hiroyuki Mishima <missy@be.to>
2011-06-09 15:10:24 +0900

    added: 293 tables in hg18/Regulation/UW Histone

---
+++ lib/bio-ucsc/hg18/wgencodeuwchipseqhotspotsrep2huvech3k4me3.rb
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# = hg18/wgencodeuwchipseqhotspotsrep2huvech3k4me3.rb
+# Copyright::
+#  Copyright (C) 2011 MISHIMA, Hiroyuki
+#                <missy at be.to / hmishima at nagasaki-u.ac.jp> 
+# License::     The Ruby licence (Ryby's / GPLv2 dual)
+#
+# = Table desfription in UCSC Table Browser
+# UW Histone - ENCODE Histone Modifications by Univ. Washington
+# ChIP-seq
+#
+# = ommitted dynamic method(s) due to the method name collision
+#  none
+
+module Bio
+  module Ucsc
+    module Hg18
+      class WgEncodeUwChIPSeqHotspotsRep2HuvecH3k4me3 < DBConnection
+        extend Bio::Ucsc::Hg18::QueryUsingChromBin
+        set_table_name 'wgEncodeUwChIPSeqHotspotsRep2HuvecH3k4me3'
+        set_primary_key nil
+      end 
+    end # module Hg18
+  end # module Ucsc
+end # Bio
---
+++ lib/bio-ucsc/hg18/wgencodeuwchipseqpeaksrep1k562h3k36me3.rb
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# = hg18/wgencodeuwchipseqpeaksrep1k562h3k36me3.rb
+# Copyright::
+#  Copyright (C) 2011 MISHIMA, Hiroyuki
+#                <missy at be.to / hmishima at nagasaki-u.ac.jp> 
+# License::     The Ruby licence (Ryby's / GPLv2 dual)
+#
+# = Table desfription in UCSC Table Browser
+# UW Histone - ENCODE Histone Modifications by Univ. Washington
+# ChIP-seq
+#
+# = ommitted dynamic method(s) due to the method name collision
+#  none
+
+module Bio
+  module Ucsc
+    module Hg18
+      class WgEncodeUwChIPSeqPeaksRep1K562H3k36me3 < DBConnection
+        extend Bio::Ucsc::Hg18::QueryUsingChromBin
+        set_table_name 'wgEncodeUwChIPSeqPeaksRep1K562H3k36me3'
+        set_primary_key nil
+      end 
+    end # module Hg18
+  end # module Ucsc
+end # Bio
