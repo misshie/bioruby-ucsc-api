619f47909953f9732fa749f217c0eb38b3b2efb7
Hiroyuki Mishima <missy@be.to>
2011-06-09 10:33:58 +0900

    added: 196 tables in hg18/Regulation/HAIB TFBS (RawSignal)

---
+++ lib/bio-ucsc/hg18/wgencodehudsonalphachipseqrawsignalrep2hepg2hey1pcr1x.rb
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# = hg18/wgencodehudsonalphachipseqrawsignalrep2hepg2hey1pcr1x.rb
+# Copyright::
+#  Copyright (C) 2011 MISHIMA, Hiroyuki
+#                <missy at be.to / hmishima at nagasaki-u.ac.jp> 
+# License::     The Ruby licence (Ryby's / GPLv2 dual)
+#
+# = Table desfription in UCSC Table Browser
+# HAIB TFBS - ENCODE Transcription Factor Binding Sites by ChIP-seq
+# from HudsonAlpha Institute
+#
+# = ommitted dynamic method(s) due to the method name collision
+#  none
+
+module Bio
+  module Ucsc
+    module Hg18
+      class WgEncodeHudsonalphaChipSeqRawSignalRep2Hepg2Hey1Pcr1x < DBConnection
+        extend Bio::Ucsc::Hg18::QueryUsingChromBin
+        set_table_name 'wgEncodeHudsonalphaChipSeqRawSignalRep2Hepg2Hey1Pcr1x'
+        set_primary_key nil
+      end 
+    end # module Hg18
+  end # module Ucsc
+end # Bio
