2ff0fe180dbe79984eaa2903302090571a5f7bb2
Hiroyuki Mishima <missy@be.to>
2011-06-09 12:49:43 +0900

    added: 158 tables in hg18/Regulation/Open Chromatin (signal/zinba)

---
+++ lib/bio-ucsc/hg18/wgencodeutachipseqsignalk562ctcfv2.rb
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# = hg18/wgencodeutachipseqsignalk562ctcfv2.rb
+# Copyright::
+#  Copyright (C) 2011 MISHIMA, Hiroyuki
+#                <missy at be.to / hmishima at nagasaki-u.ac.jp> 
+# License::     The Ruby licence (Ryby's / GPLv2 dual)
+#
+# = Table desfription in UCSC Table Browser
+# Open Chromatin - ENCODE Open Chromatin, Duke/UNC/UT
+#
+# = ommitted dynamic method(s) due to the method name collision
+#  none
+
+module Bio
+  module Ucsc
+    module Hg18
+      class WgEncodeUtaChIPseqSignalK562CtcfV2 < DBConnection
+        extend Bio::Ucsc::Hg18::QueryUsingChromBin
+        set_table_name 'wgEncodeUtaChIPseqSignalK562CtcfV2'
+        set_primary_key nil
+      end 
+    end # module Hg18
+  end # module Ucsc
+end # Bio
