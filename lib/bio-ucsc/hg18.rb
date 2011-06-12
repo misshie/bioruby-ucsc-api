4f288b5fd047ec27f9a333b507a25a622140c7ec
Hiroyuki Mishima <missy@be.to>
2011-06-12 09:58:54 +0900

    added: hg18/netcanfam2.rb

--- lib/bio-ucsc/hg18.rb
+++ lib/bio-ucsc/hg18.rb
@@ -2940,7 +2940,8 @@ module Bio
       autoload :NetMm9, "#{base}/netmm9"
       ## track: Dog Chain/Net
       autoload :ChainCanFam2, "#{base}/chaincanfam2"
-
+      autoload :NetCanFam2, "#{base}/netcanfam2"
+      
 
 
 
