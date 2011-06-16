29c8778c3951da380411fdc99e3b8341369769e5
Hiroyuki Mishima <missy@be.to>
2011-06-17 08:48:12 +0900

    added: 6 tables in hg18/PilotEncode/AffyRNASignal

---
+++ devtool/hg18/AffyRNASignal/autoload_snippet.rb
@@ -0,0 +1,7 @@
+      ## track: Affy RNA Signal
+      autoload :EncodeAffyRnaGm06990Signal, "#{base}/encodeaffyrnagm06990signal"
+      autoload :EncodeAffyRnaHeLaSignal, "#{base}/encodeaffyrnahelasignal"
+      autoload :EncodeAffyRnaHl60SignalHr00, "#{base}/encodeaffyrnahl60signalhr00"
+      autoload :EncodeAffyRnaHl60SignalHr02, "#{base}/encodeaffyrnahl60signalhr02"
+      autoload :EncodeAffyRnaHl60SignalHr08, "#{base}/encodeaffyrnahl60signalhr08"
+      autoload :EncodeAffyRnaHl60SignalHr32, "#{base}/encodeaffyrnahl60signalhr32"
