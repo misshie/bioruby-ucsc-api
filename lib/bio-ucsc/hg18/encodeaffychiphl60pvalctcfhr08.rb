6f1bbade27688bae290aa7582d7928c381dbeec8
Hiroyuki Mishima <missy@be.to>
2011-06-17 12:51:44 +0900

    added: 41 tables in hg18/PilotENCODE/Affy pVal

---
+++ lib/bio-ucsc/hg18/encodeaffychiphl60pvalctcfhr08.rb
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# = hg18/encodeaffychiphl60pvalctcfhr08.rb
+# Copyright::
+#  Copyright (C) 2011 MISHIMA, Hiroyuki
+#                <missy at be.to / hmishima at nagasaki-u.ac.jp> 
+# License::     The Ruby licence (Ryby's / GPLv2 dual)
+#
+# = Table desfription in UCSC Table Browser
+# Affy pVal - Affymetrix ChIP-chip (retinoic acid-treated HL-60 cells) P-Value
+#
+# = ommitted dynamic method(s) due to the method name collision
+#  none
+
+module Bio
+  module Ucsc
+    module Hg18
+      class EncodeAffyChIpHl60PvalCtcfHr08 < DBConnection
+        extend Bio::Ucsc::Hg18::QueryUsingChromBin
+        set_table_name 'encodeAffyChIpHl60PvalCtcfHr08'
+        set_primary_key nil
+      end 
+    end # module Hg18
+  end # module Ucsc
+end # Bio
