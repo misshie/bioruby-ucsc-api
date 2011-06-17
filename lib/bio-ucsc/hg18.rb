4124450eae1c75d50a9a8653d900430024f56aa2
Hiroyuki Mishima <missy@be.to>
2011-06-17 11:01:11 +0900

    added: 6 tables in hg18/PilotENCODE/Yale MAS TAR

--- lib/bio-ucsc/hg18.rb
+++ lib/bio-ucsc/hg18.rb
@@ -3333,6 +3333,13 @@ module Bio
       autoload :EncodeYaleMASPlacRNANprotTMREVMless36mer36bp, "#{base}/encodeyalemasplacrnanprottmrevmless36mer36bp"
       autoload :EncodeYaleMASPlacRNATransMapFwdMless36mer36bp, "#{base}/encodeyalemasplacrnatransmapfwdmless36mer36bp"
       autoload :EncodeYaleMASPlacRNATransMapRevMless36mer36bp, "#{base}/encodeyalemasplacrnatransmaprevmless36mer36bp"
+      ## track: Yale MAS TAR
+      autoload :EncodeYaleMASNB4RNANProtTarsFWDMless36mer36bp, "#{base}/encodeyalemasnb4rnanprottarsfwdmless36mer36bp"
+      autoload :EncodeYaleMASNB4RNANProtTarsREVMless36mer36bp, "#{base}/encodeyalemasnb4rnanprottarsrevmless36mer36bp"
+      autoload :EncodeYaleMASPlacRNANprotTarsFWDMless36mer36bp, "#{base}/encodeyalemasplacrnanprottarsfwdmless36mer36bp"
+      autoload :EncodeYaleMASPlacRNANprotTarsREVMless36mer36bp, "#{base}/encodeyalemasplacrnanprottarsrevmless36mer36bp"
+      autoload :EncodeYaleMASPlacRNATarsFwdMless36mer36bp, "#{base}/encodeyalemasplacrnatarsfwdmless36mer36bp"
+      autoload :EncodeYaleMASPlacRNATarsRevMless36mer36bp, "#{base}/encodeyalemasplacrnatarsrevmless36mer36bp"
 
       
 
