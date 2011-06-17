bb2c6b532dbc68864631b692b0deafaddb899cae
Hiroyuki Mishima <missy@be.to>
2011-06-17 09:08:09 +0900

    added: 6 tables in hg18/PilotENCODE/Affy Transfrags

---
+++ devtool/hg18/AffyTransfrags/autoload_snippet.rb
@@ -0,0 +1,7 @@
+      ## track: Affy RNA Transfrags
+      autoload :EncodeAffyRnaGm06990Sites, "#{base}/encodeaffyrnagm06990sites"
+      autoload :EncodeAffyRnaHeLaSites, "#{base}/encodeaffyrnahelasites"
+      autoload :EncodeAffyRnaHl60SitesHr00, "#{base}/encodeaffyrnahl60siteshr00"
+      autoload :EncodeAffyRnaHl60SitesHr02, "#{base}/encodeaffyrnahl60siteshr02"
+      autoload :EncodeAffyRnaHl60SitesHr08, "#{base}/encodeaffyrnahl60siteshr08"
+      autoload :EncodeAffyRnaHl60SitesHr32, "#{base}/encodeaffyrnahl60siteshr32"
--- lib/bio-ucsc/hg18.rb
+++ lib/bio-ucsc/hg18.rb
@@ -3285,8 +3285,13 @@ module Bio
       autoload :EncodeAffyRnaHl60SignalHr02, "#{base}/encodeaffyrnahl60signalhr02"
       autoload :EncodeAffyRnaHl60SignalHr08, "#{base}/encodeaffyrnahl60signalhr08"
       autoload :EncodeAffyRnaHl60SignalHr32, "#{base}/encodeaffyrnahl60signalhr32"
-
-
+      ## track: Affy RNA Transfrags
+      autoload :EncodeAffyRnaGm06990Sites, "#{base}/encodeaffyrnagm06990sites"
+      autoload :EncodeAffyRnaHeLaSites, "#{base}/encodeaffyrnahelasites"
+      autoload :EncodeAffyRnaHl60SitesHr00, "#{base}/encodeaffyrnahl60siteshr00"
+      autoload :EncodeAffyRnaHl60SitesHr02, "#{base}/encodeaffyrnahl60siteshr02"
+      autoload :EncodeAffyRnaHl60SitesHr08, "#{base}/encodeaffyrnahl60siteshr08"
+      autoload :EncodeAffyRnaHl60SitesHr32, "#{base}/encodeaffyrnahl60siteshr32"
 
 
 
