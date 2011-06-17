87cdbb55bb894252ac377cd6a0e1146d1e37e1fd
Hiroyuki Mishima <missy@be.to>
2011-06-17 17:08:51 +0900

    added: 14 tables in hg18/PilotENCODE/Affy Strict Sig

--- lib/bio-ucsc/hg18.rb
+++ lib/bio-ucsc/hg18.rb
@@ -3520,6 +3520,27 @@ module Bio
       autoload :EncodeAffyChIpHl60PvalStrictPol2Hr32, "#{base}/encodeaffychiphl60pvalstrictpol2hr32"
       autoload :EncodeAffyChIpHl60PvalStrictp63_ActD, "#{base}/encodeaffychiphl60pvalstrictp63_actd"
       autoload :EncodeAffyChIpHl60PvalStrictp63_mActD, "#{base}/encodeaffychiphl60pvalstrictp63_mactd"
+      ## track: Affy Strict Sig
+      autoload :EncodeAffyChIpHl60SignalStrictH3K9K14DHr00, "#{base}/encodeaffychiphl60signalstricth3k9k14dhr00"
+      autoload :EncodeAffyChIpHl60SignalStrictH3K9K14DHr02, "#{base}/encodeaffychiphl60signalstricth3k9k14dhr02"
+      autoload :EncodeAffyChIpHl60SignalStrictH3K9K14DHr08, "#{base}/encodeaffychiphl60signalstricth3k9k14dhr08"
+      autoload :EncodeAffyChIpHl60SignalStrictH3K9K14DHr32, "#{base}/encodeaffychiphl60signalstricth3k9k14dhr32"
+      autoload :EncodeAffyChIpHl60SignalStrictHisH4Hr00, "#{base}/encodeaffychiphl60signalstricthish4hr00"
+      autoload :EncodeAffyChIpHl60SignalStrictHisH4Hr02, "#{base}/encodeaffychiphl60signalstricthish4hr02"
+      autoload :EncodeAffyChIpHl60SignalStrictHisH4Hr08, "#{base}/encodeaffychiphl60signalstricthish4hr08"
+      autoload :EncodeAffyChIpHl60SignalStrictHisH4Hr32, "#{base}/encodeaffychiphl60signalstricthish4hr32"
+      autoload :EncodeAffyChIpHl60SignalStrictPol2Hr00, "#{base}/encodeaffychiphl60signalstrictpol2hr00"
+      autoload :EncodeAffyChIpHl60SignalStrictPol2Hr02, "#{base}/encodeaffychiphl60signalstrictpol2hr02"
+      autoload :EncodeAffyChIpHl60SignalStrictPol2Hr08, "#{base}/encodeaffychiphl60signalstrictpol2hr08"
+      autoload :EncodeAffyChIpHl60SignalStrictPol2Hr32, "#{base}/encodeaffychiphl60signalstrictpol2hr32"
+      autoload :EncodeAffyChIpHl60SignalStrictp63_ActD, "#{base}/encodeaffychiphl60signalstrictp63_actd"
+      autoload :EncodeAffyChIpHl60SignalStrictp63_mActD, "#{base}/encodeaffychiphl60signalstrictp63_mactd"
+
+
+
+
+
+
 
     end
   end
---
+++ lib/bio-ucsc/hg18/encodeaffychiphl60signalstrictpol2hr08.rb
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# = hg18/encodeaffychiphl60signalstrictpol2hr08.rb
+# Copyright::
+#  Copyright (C) 2011 MISHIMA, Hiroyuki
+#                <missy at be.to / hmishima at nagasaki-u.ac.jp> 
+# License::     The Ruby licence (Ryby's / GPLv2 dual)
+#
+# = Table desfription in UCSC Table Browser
+# Affy Strict Sig - Affymetrix ChIP-chip (HL-60 and ME-180 cells)
+# Strict Signal
+#
+# = ommitted dynamic method(s) due to the method name collision
+#  none
+
+module Bio
+  module Ucsc
+    module Hg18
+      class EncodeAffyChIpHl60SignalStrictPol2Hr08 < DBConnection
+        extend Bio::Ucsc::Hg18::QueryUsingChromBin
+        set_table_name 'encodeAffyChIpHl60SignalStrictPol2Hr08'
+        set_primary_key nil
+      end 
+    end # module Hg18
+  end # module Ucsc
+end # Bio
