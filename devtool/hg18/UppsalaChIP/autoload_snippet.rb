07c285e4cd24e237c656770e2068546635b3cec8
Hiroyuki Mishima <missy@be.to>
2011-06-19 10:09:28 +0900

    added: 5 tables in hg18/PilotENCODE/Uppsala ChIP

---
+++ devtool/hg18/UppsalaChIP/autoload_snippet.rb
@@ -0,0 +1,6 @@
+      ## track: Uppsala ChIP
+      autoload :EncodeUppsalaChipAch3, "#{base}/encodeuppsalachipach3"
+      autoload :EncodeUppsalaChipHnf3b, "#{base}/encodeuppsalachiphnf3b"
+      autoload :EncodeUppsalaChipHnf4a, "#{base}/encodeuppsalachiphnf4a"
+      autoload :EncodeUppsalaChipUsf1, "#{base}/encodeuppsalachipusf1"
+      autoload :EncodeUppsalaChipSites, "#{base}/encodeuppsalachipsites"
