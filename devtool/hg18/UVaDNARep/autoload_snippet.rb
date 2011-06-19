03f9ce2884efd40e0fba0ff093877bacb5a4aa9a
Hiroyuki Mishima <missy@be.to>
2011-06-19 14:01:34 +0900

    added: 5 tables in hg18/PilotENCOCE/UVa DNA Rep

---
+++ devtool/hg18/UVaDNARep/autoload_snippet.rb
@@ -0,0 +1,6 @@
+      ## track: UVa DNA Rep
+      autoload :EncodeUvaDnaRep0, "#{base}/encodeuvadnarep0"
+      autoload :EncodeUvaDnaRep2, "#{base}/encodeuvadnarep2"
+      autoload :EncodeUvaDnaRep4, "#{base}/encodeuvadnarep4"
+      autoload :EncodeUvaDnaRep6, "#{base}/encodeuvadnarep6"
+      autoload :EncodeUvaDnaRep8, "#{base}/encodeuvadnarep8"
--- lib/bio-ucsc/hg18.rb
+++ lib/bio-ucsc/hg18.rb
@@ -3817,6 +3817,13 @@ module Bio
       autoload :EncodeUncFaireSignal, "#{base}/encodeuncfairesignal"
       autoload :EncodeUncFairePeaks,  "#{base}/encodeuncfairepeaks"
       autoload :EncodeUncFairePeaksChipotle,  "#{base}/encodeuncfairepeakschipotle"
+      ## track: UVa DNA Rep
+      autoload :EncodeUvaDnaRep0, "#{base}/encodeuvadnarep0"
+      autoload :EncodeUvaDnaRep2, "#{base}/encodeuvadnarep2"
+      autoload :EncodeUvaDnaRep4, "#{base}/encodeuvadnarep4"
+      autoload :EncodeUvaDnaRep6, "#{base}/encodeuvadnarep6"
+      autoload :EncodeUvaDnaRep8, "#{base}/encodeuvadnarep8"
+
     end
   end
 end
