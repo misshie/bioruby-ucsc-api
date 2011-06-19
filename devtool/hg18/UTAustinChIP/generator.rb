ed62a8e783ba144d44ff9265ff1b687e59e48f32
Hiroyuki Mishima <missy@be.to>
2011-06-19 09:39:28 +0900

    8 tables in hg18/PilotENCODE/UT-Austin ChIP

---
+++ devtool/hg18/UTAustinChIP/generator.rb
@@ -0,0 +1,51 @@
+#!/usr/local/bin/ruby-1.9
+
+require 'erb'
+require 'fileutils'
+
+HEADER = "      ## track: UT-Austin ChIP"
+
+TABLES = %w(
+encodeUtexChipHeLaMycRaw
+encodeUtexChip2091fibMycRaw
+encodeUtexChip2091fibMycStimRaw
+encodeUtexChip2091fibE2F4Raw
+encodeUtexChipHeLaMycPeaks
+encodeUtexChip2091fibMycPeaks
+encodeUtexChip2091fibMycStimPeaks
+encodeUtexChip2091fibE2F4Peaks
+)
+
+temp_spec = "template_spec.txt"
+temp_auto = "template_auto.txt"
+temp_main = "template_main.txt"
+
+FileUtils.mkdir_p("Specs") unless FileTest.exist?("Specs")
+FileUtils.mkdir_p("Libs") unless FileTest.exist?("Libs")
+
+TABLES.each do |tab|
+  table_name  = tab
+  klass_name  = tab[0].upcase << tab[1..-1]
+  script_name = tab.downcase
+
+  open("Specs/#{script_name}_spec.rb", "w") do |writer|
+    erb = ERB.new File.read(temp_spec)
+    writer.puts erb.result binding
+  end
+
+  open("Libs/#{script_name}.rb", "w") do |writer|
+    erb = ERB.new File.read(temp_main)
+    writer.puts erb.result binding
+  end
+end
+
+open("autoload_snippet.rb", 'w') do |writer|
+  writer.puts HEADER
+  TABLES.each do |tab|
+    table_name  = tab
+    klass_name  = tab[0].upcase << tab[1..-1]
+    script_name = tab.downcase
+    erb = ERB.new File.read(temp_auto)
+    writer.puts erb.result binding
+  end
+end
--- lib/bio-ucsc/hg18.rb
+++ lib/bio-ucsc/hg18.rb
@@ -3681,7 +3681,16 @@ module Bio
       ## track: UCD Ng ChIP Hits
       autoload :EncodeUcDavisChipHitsE2F1, "#{base}/encodeucdavischiphitse2f1"
       autoload :EncodeUcDavisChipHitsMyc, "#{base}/encodeucdavischiphitsmyc"
-
+      ## track: UT-Austin ChIP
+      autoload :EncodeUtexChipHeLaMycRaw, "#{base}/encodeutexchiphelamycraw"
+      autoload :EncodeUtexChip2091fibMycRaw, "#{base}/encodeutexchip2091fibmycraw"
+      autoload :EncodeUtexChip2091fibMycStimRaw, "#{base}/encodeutexchip2091fibmycstimraw"
+      autoload :EncodeUtexChip2091fibE2F4Raw, "#{base}/encodeutexchip2091fibe2f4raw"
+      autoload :EncodeUtexChipHeLaMycPeaks, "#{base}/encodeutexchiphelamycpeaks"
+      autoload :EncodeUtexChip2091fibMycPeaks, "#{base}/encodeutexchip2091fibmycpeaks"
+      autoload :EncodeUtexChip2091fibMycStimPeaks, "#{base}/encodeutexchip2091fibmycstimpeaks"
+      autoload :EncodeUtexChip2091fibE2F4Peaks, "#{base}/encodeutexchip2091fibe2f4peaks"
+ 
 
 
 
