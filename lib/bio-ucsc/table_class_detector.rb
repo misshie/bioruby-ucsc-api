2b9ab7db951c2771060bde7e6af41fdbf93aa2cd
Hiroyuki Mishima <missy@be.to>
2011-08-05 09:36:46 +0900

    Just before switching to the automatic table detection approach

---
+++ lib/bio-ucsc/table_class_detector.rb
@@ -0,0 +1,299 @@
+#
+# = table_class_generator.rb
+#
+# Copyright::   Copyright (C) 2011
+#               MISHIMA, Hiroyuki
+#               <missy at be.to / hmishima at nagasaki-u.ac.jp> 
+# License::     The Ruby licence (Ryby's / GPLv2 dual)
+#
+# == supported table categories and arguments/defaults
+# psl :tableName, :bin => true
+# bed :tableName, :bin => true
+# genepred :tableName, :bin=> true
+# genetic :tableName
+
+module Bio 
+  module Ucsc
+    module TableClassDetector
+
+      RESERVED_METHODS = 
+        ['valid', 'validate', 'class', 'method', 'methods', 'type']
+
+      def const_missing(sym)
+        module_eval generic(sym)
+        col_names = const_get(sym).columns.map{|x|x.name}
+
+        case
+        when (["bin", "tName", "tStart", "tEnd"] - col_names).empty?
+          module_eval psl(sym, :bin => true)
+          const_get(sym)
+
+        when (["bin", "tName", "tStart", "tEnd"] - col_names) == ["bin"]
+          module_eval psl(sym, :bin => false)
+          const_get(sym)
+
+        when (["bin", "chrom", "chromStart", "chromEnd"] - col_names).empty?
+          module_eval bed(sym, :bin => true)
+          const_get(sym)
+
+        when (["bin", "chrom", "chromStart", "chromEnd"] - col_names) == ["bin"]
+          module_eval bed(sym, :bin => false)
+          const_get(sym)
+
+        when (["bin", "chrom", "txStart", "txEnd"] - col_names).empty?
+          module_eval genepred(sym, :bin => true)
+          const_get(sym)
+
+        when (["bin", "chrom", "txStart", "txEnd"] - col_names)== ["bin"]
+          module_eval genepred(sym, :bin => false)
+          const_get(sym)
+
+        else
+          const_get(sym)
+        end
+      end
+
+      # generic: tables without interval serarch supported
+      # also used to prefetch table column names
+      def generic(sym)
+        %!
+          class #{uphead(sym)} < DBConnection
+            set_table_name "#{downhead(sym)}"
+            set_primary_key nil
+            #{delete_reserved_methods}
+          end
+        !
+      end # def generic
+
+      # PSL: Pattern Space Layout
+      # interval search using tName/tStart/tEnd
+      def psl(sym, opts={:bin => true})
+        case opts[:bin]
+        when true
+          %!
+            class #{uphead(sym)} < DBConnection
+              set_table_name "#{downhead(sym)}"
+              set_primary_key nil
+              #{delete_reserved_methods}
+              def self.find_by_interval(interval, opt = {:partial => true})
+                find_first_or_all_by_interval(interval, :first, opt)
+              end
+        
+              def self.find_all_by_interval(interval, opt = {:partial => true})
+                find_first_or_all_by_interval(interval, :all, opt)
+              end
+
+              def self.find_first_or_all_by_interval(interval, first_all, opt)
+                zstart = interval.zero_start
+                zend   = interval.zero_end
+
+                if opt[:partial] == true
+                  where = <<-SQL
+    tName = :chrom
+AND bin in (:bins)
+AND ((tStart BETWEEN :zstart AND :zend)
+ OR (tEnd BETWEEN :zstart AND :zend)
+ OR (tStart <= :zstart AND tEnd >= :zend))
+                SQL
+                else
+            where = <<-SQL
+    tName = :chrom 
+AND ((tStart BETWEEN :zstart AND :zend)
+AND  (tEnd BETWEEN :zstart AND :zend))
+                SQL
+                end
+                cond = {
+                :chrom => interval.chrom,
+                :bins  => Ucsc::UcscBin.bin_all(zstart, zend),
+                :zstart => zstart,
+                :zend => zend,
+              }
+                self.find(first_all,
+                          :select => "*",
+                          :conditions => [where, cond],
+                          )
+              end
+            end
+          !
+        when false
+          raise NotImplementedError # To Be Implemented
+        end
+      end # def psl
+
+      # BED: Browser Extensible Description format
+      # interval search using chrom/chromStart/chromEnd
+      def bed(sym, opts={:bin => true})
+        case opts[:bin]
+        when true
+          %!
+            class #{uphead(sym)} < DBConnection
+              set_table_name "#{downhead(sym)}"
+              set_primary_key nil
+              #{delete_reserved_methods}
+ 
+              def self.find_by_interval(interval, opt = {:partial => true})
+                find_first_or_all_by_interval(interval, :first, opt)
+              end
+        
+              def self.find_all_by_interval(interval, opt = {:partial => true})
+                find_first_or_all_by_interval(interval, :all, opt)
+              end
+            
+              def self.find_first_or_all_by_interval(interval, first_all, opt)
+                zstart = interval.zero_start
+                zend   = interval.zero_end
+
+                if opt[:partial] == true
+                  where = <<-SQL
+    chrom = :chrom
+AND bin in (:bins)
+AND ((chromStart BETWEEN :zstart AND :zend)
+ OR (chromEnd BETWEEN :zstart AND :zend)
+ OR (chromStart <= :zstart AND chromEnd >= :zend))
+                SQL
+                else
+                  where = <<-SQL
+    chrom = :chrom 
+AND bin in (:bins)
+AND ((chromStart BETWEEN :zstart AND :zend)
+AND  (chromEnd BETWEEN :zstart AND :zend))
+                SQL
+                end
+                cond = {
+                :chrom => interval.chrom,
+                :bins  => Ucsc::UcscBin.bin_all(zstart, zend),
+                :zstart => zstart,
+                :zend => zend,
+              }          
+                self.find(first_all,
+                          :select => "*",
+                          :conditions => [where, cond],
+                          )
+              end
+            end
+          !
+        when false
+          %!
+            class #{uphead(sym)} < DBConnection
+              set_table_name "#{downhead(sym)}"
+              set_primary_key nil
+              #{delete_reserved_methods}
+              
+              def self.find_by_interval(interval, opt = {:partial => true})
+                find_first_or_all_by_interval(interval, :first, opt)
+              end
+              
+              def self.find_all_by_interval(interval, opt = {:partial => true})
+                find_first_or_all_by_interval(interval, :all, opt)
+              end
+
+              def self.find_first_or_all_by_interval(interval, first_all, opt)
+                zstart = interval.zero_start
+                zend   = interval.zero_end
+                if opt[:partial] == true
+                  where = <<-SQL
+        chrom = :chrom
+  AND ((chromStart BETWEEN :zstart AND :zend)
+  OR   (chromEnd BETWEEN :zstart AND :zend)
+  OR   (chromStart <= :zstart AND chromEnd >= :zend))
+                SQL
+                else
+                  where = <<-SQL
+        chrom = :chrom 
+  AND ((chromStart BETWEEN :zstart AND :zend)
+  AND  (chromEnd BETWEEN :zstart AND :zend))
+                SQL
+                end 
+                cond = {
+                :chrom => interval.chrom,
+                :zstart => zstart,
+                :zend => zend,
+              }
+                self.find(first_all,
+                          :select => "*",
+                          :conditions => [where, cond],
+                          )
+              end
+            end
+          !
+        end # case opts[:bin]
+      end # def bed
+
+     # genePred: Gene and gene-prediction features
+      # interval search using chrom/txStart/txEnd
+      def genepred(sym, opts={:bin => true})
+        case opts[:bin]
+        when true
+          %!
+            class #{uphead(sym)} < DBConnection
+              set_table_name "#{downhead(sym)}"
+              set_primary_key nil
+              #{delete_reserved_methods}
+ 
+              def self.find_by_interval(interval, opt = {:partial => true})
+                find_first_or_all_by_interval(interval, :first, opt)
+              end
+        
+              def self.find_all_by_interval(interval, opt = {:partial => true})
+                find_first_or_all_by_interval(interval, :all, opt)
+              end
+
+              def self.find_first_or_all_by_interval(interval, first_all, opt)
+                zstart = interval.zero_start
+                zend   = interval.zero_end
+                if opt[:partial] == true
+                  where = <<-SQL
+      chrom = :chrom
+AND   bin in (:bins)
+AND ((txStart BETWEEN :zstart AND :zend)
+ OR  (txEnd BETWEEN :zstart AND :zend)
+ OR  (txStart <= :zstart AND txEnd >= :zend))
+                SQL
+                else
+                  where = <<-SQL
+      chrom = :chrom
+AND   bin in (:bins)
+AND ((txStart BETWEEN :zstart AND :zend)
+AND  (txEnd BETWEEN :zstart AND :zend))
+                SQL
+                end
+                cond = {
+                :chrom  => interval.chrom,
+                :bins   => Bio::Ucsc::UcscBin.bin_all(zstart, zend),
+                :zstart => zstart,
+                :zend   => zend,
+              }
+                self.find(first_all,
+                          :select => "*",
+                          :conditions => [where, cond],
+                          )
+              end
+            end
+          !
+
+        when false
+          raise NotImplementedError # TO BE IMPLEMENTED
+        end
+      end # def genepred
+
+      private
+
+      def delete_reserved_methods
+        codes = Array.new
+        RESERVED_METHODS.each do |reserved|
+          codes << "columns_hash.delete('#{reserved}')"
+        end
+        codes.join("\n")
+      end
+
+      def uphead(sym)
+        (sym.to_s[0..0].upcase + sym[1..-1])
+      end
+
+      def downhead(sym)
+        (sym.to_s[0..0].downcase + sym[1..-1])
+      end
+
+    end # TableClassGenerator
+  end # module Ucsc
+end # module Bio
