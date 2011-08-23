ddc2f7df053045c7c46b7cee0df228a781551abb
Hiroyuki Mishima <missy@be.to>
2011-08-23 10:38:32 +0900

    update: README.rdoc - description of sample scripts

--- samples/bed2refseq.rb
+++ samples/bed2refseq.rb
@@ -3,19 +3,21 @@
 #require 'bio-ucsc'
 require '../lib/bio-ucsc'
 
-include Bio::Ucsc::Hg19
+include Bio
 
-DBConnection.connect
+Ucsc::Hg19::DBConnection.connect
 
 genes = Array.new
 ARGF.each_line do |row|
   row.chomp!
   next if row.empty?
   chr, chr_start, chr_end = row.split("\t")
-  interval = Bio::GenomicInterval.zero_based(chr,
-                                             Integer(chr_start),
-                                             Integer(chr_end))
-  genes.concat(RefGene.find_all_by_interval(interval).map{|e|e.name2}) 
+  gi = GenomicInterval.zero_based(chr,
+                                  Integer(chr_start),
+                                  Integer(chr_end),)
+
+  results = Ucsc::Hg19::RefGene.with_interval(gi).select(:name2).find(:all)
+  genes.concat(results.map{|e|e.name2})
 end
 
-genes.uniq!.each{|e|puts e}
+genes.uniq.each{|e|puts e} if genes
