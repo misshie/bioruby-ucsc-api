ee548b7f0d6e14426e202bd6f5e0bbb9d71a8ff1
Hiroyuki Mishima <missy@be.to>
2012-01-17 10:21:58 +0900

    lib/bio-ucsc/table_assoc_detector.rb added.. but not used.

---
+++ lib/bio-ucsc/table_assoc_detector.rb
@@ -0,0 +1,22 @@
+#
+# = table_assoc_detector.rb
+#
+# Copyright::   Copyright (C) 2012
+#               MISHIMA, Hiroyuki
+#               <missy at be.to / hmishima at nagasaki-u.ac.jp> 
+# License::     The Ruby licence (Ryby's / GPLv2 dual)
+#
+# == detect reference of table association and define it automatically
+# To be included in each table class
+#
+
+module Bio
+  module Ucsc
+    module TableAssocDetector
+      def method_missing(method, *args)
+        
+      end
+    end
+  end
+end
+
