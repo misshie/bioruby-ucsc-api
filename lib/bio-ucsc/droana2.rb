f91a889cda966b8edac9ce0ea2cea73be12ca97d
Hiroyuki Mishima <missy@be.to>
2012-02-10 11:19:13 +0900

    update droana2

--- lib/bio-ucsc/droana2.rb
+++ lib/bio-ucsc/droana2.rb
@@ -1,12 +1,12 @@
 #
 # = AUTOMATIC Table Definition of the D. ananassae Aug. 2005 (Agencourt prelim/droAna2) assembly
-# Copyright::   Copyright (C) 2011
+# Copyright::   Copyright (C) 2011, 2012
 #               MISHIMA, Hiroyuki
 #               <missy at be.to / hmishima at nagasaki-u.ac.jp> 
 # License::     The Ruby licence (Ryby's / GPLv2 dual)
 #
 
-require "#{::File.dirname(__FILE__)}/droana2/db_connection"
+require "#{::File.dirname(__FILE__)}/db_connector"
 require "#{::File.dirname(__FILE__)}/table_class_detector"
 
 module Bio
@@ -15,6 +15,8 @@ module Bio
       CHROMS = %w() # manu scaffolds
 
       extend TableClassDetector
+      include DBConnector
+      DBConnection.database "droAna2"
 
       base = "#{::File.dirname(__FILE__)}/droana2"
     end
