53a72e16b03f5e2b2ffb9198859848dd5661eadc
Hiroyuki Mishima <missy@be.to>
2012-02-10 09:08:39 +0900

    update ci2

--- lib/bio-ucsc/ci2.rb
+++ lib/bio-ucsc/ci2.rb
@@ -6,7 +6,7 @@
 # License::     The Ruby licence (Ryby's / GPLv2 dual)
 #
 
-require "#{::File.dirname(__FILE__)}/ci2/db_connection"
+require "#{::File.dirname(__FILE__)}/db_connector"
 require "#{::File.dirname(__FILE__)}/table_class_detector"
 
 module Bio
@@ -15,7 +15,10 @@ module Bio
       CHROMS = 
 %w(chr01p chr01q chr02q chr03p chr03q chr04q chr05q chr06q chr07q chr08q
 chr09p chr09q chr10p chr10q chr12p chr12q chr13q chr14p chr14q)
+
       extend TableClassDetector
+      include DBConnector
+      DBConnection.database "ci2"
 
       base = "#{::File.dirname(__FILE__)}/ci2"
     end
