02d1c78997d777f47ac31103be8ca3d122eb2bca
Hiroyuki Mishima <missy@be.to>
2012-02-10 11:35:07 +0900

    update droper1

--- lib/bio-ucsc/droper1.rb
+++ lib/bio-ucsc/droper1.rb
@@ -1,20 +1,22 @@
 #
 # = AUTOMATIC Table Definition of the D. persimilis Oct. 2005 (Broad/droPer1) assembly
-# Copyright::   Copyright (C) 2011
+# Copyright::   Copyright (C) 2011, 2012
 #               MISHIMA, Hiroyuki
 #               <missy at be.to / hmishima at nagasaki-u.ac.jp> 
 # License::     The Ruby licence (Ryby's / GPLv2 dual)
 #
 
-require "#{::File.dirname(__FILE__)}/droper1/db_connection"
+require "#{::File.dirname(__FILE__)}/db_connector"
 require "#{::File.dirname(__FILE__)}/table_class_detector"
 
 module Bio
   module Ucsc
     module DroPer1
-      CHROMS = %w() # manu 'super' contigs
+      CHROMS = %w() # many 'super' contigs
 
       extend TableClassDetector
+      include DBConnector
+      DBConnection.database "droPer1"
 
       base = "#{::File.dirname(__FILE__)}/droper1"
     end
