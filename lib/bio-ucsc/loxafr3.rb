5afedaeecf740f6d55a9fa3498c227a1210eee29
Hiroyuki Mishima <missy@be.to>
2012-02-13 09:37:48 +0900

    update loxafr3

--- lib/bio-ucsc/loxafr3.rb
+++ lib/bio-ucsc/loxafr3.rb
@@ -1,12 +1,12 @@
 #
 # = AUTOMATIC Table Definition of the Elephant Jul. 2009 (Broad/loxAfr3) assembly
-# Copyright::   Copyright (C) 2011
+# Copyright::   Copyright (C) 2012
 #               MISHIMA, Hiroyuki
 #               <missy at be.to / hmishima at nagasaki-u.ac.jp> 
 # License::     The Ruby licence (Ryby's / GPLv2 dual)
 #
 
-require "#{::File.dirname(__FILE__)}/loxafr3/db_connection"
+require "#{::File.dirname(__FILE__)}/db_connector"
 require "#{::File.dirname(__FILE__)}/table_class_detector"
 
 module Bio
@@ -14,6 +14,9 @@ module Bio
     module LoxAfr3
       CHROMS = %w() # many scaffolds
       extend TableClassDetector
+      include DBConnector
+      DBConnection.database "loxAfr3"
+
       base = "#{::File.dirname(__FILE__)}/loxafr3"
     end
   end
