2280c2784f4ec1a62a2ceb5fea054e5a4ac50650
Hiroyuki Mishima <missy@be.to>
2012-02-14 09:11:57 +0900

    update pantro3

--- lib/bio-ucsc/pantro3.rb
+++ lib/bio-ucsc/pantro3.rb
@@ -1,12 +1,12 @@
 #
 # = AUTOMATIC Table Definition of the Chimp Oct. 2010 (CGSC 2.1.3/panTro3) assembly
-# Copyright::   Copyright (C) 2011
+# Copyright::   Copyright (C) 2011, 2012
 #               MISHIMA, Hiroyuki
 #               <missy at be.to / hmishima at nagasaki-u.ac.jp> 
 # License::     The Ruby licence (Ryby's / GPLv2 dual)
 #
 
-require "#{::File.dirname(__FILE__)}/pantro3/db_connection"
+require "#{::File.dirname(__FILE__)}/db_connector"
 require "#{::File.dirname(__FILE__)}/table_class_detector"
 
 module Bio
@@ -17,7 +17,9 @@ module Bio
 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20
 chr21 chr22 chrX chrY)
       extend TableClassDetector
-
+      include DBConnector
+      DBConnection.database "panTro3"
+      
       base = "#{::File.dirname(__FILE__)}/pantro3"
     end
   end
