fdb528516b843555de856f957c7ad61d78a56e28
Hiroyuki Mishima <missy@be.to>
2012-02-14 12:52:01 +0900

    update uniProt, but any tables seem to have gone from the server

--- lib/bio-ucsc/uniprot.rb
+++ lib/bio-ucsc/uniprot.rb
@@ -1,18 +1,21 @@
 #
 # = AUTOMATIC Table Definition of Uniprot
-# Copyright::   Copyright (C) 2011
+# Copyright::   Copyright (C) 2011, 2012
 #               MISHIMA, Hiroyuki
 #               <missy at be.to / hmishima at nagasaki-u.ac.jp> 
 # License::     The Ruby licence (Ryby's / GPLv2 dual)
 #
 
-require "#{::File.dirname(__FILE__)}/uniprot/db_connection"
+require "#{::File.dirname(__FILE__)}/db_connector"
 require "#{::File.dirname(__FILE__)}/table_class_detector"
 
 module Bio
   module Ucsc
     module UniProt
       extend TableClassDetector
+      include DBConnector
+      DBConnection.database "uniProt"
+
       base = "#{::File.dirname(__FILE__)}/uniprot"
     end
   end
