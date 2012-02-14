3c86a87f5f7514f89bd419721bee121539d95f33
Hiroyuki Mishima <missy@be.to>
2012-02-14 09:42:23 +0900

    update proteome, but Proteaome DB seems not to exit now

--- lib/bio-ucsc/proteome.rb
+++ lib/bio-ucsc/proteome.rb
@@ -1,18 +1,21 @@
 #
 # = AUTOMATIC Table Definition of Proteome
-# Copyright::   Copyright (C) 2011
+# Copyright::   Copyright (C) 2011, 2012
 #               MISHIMA, Hiroyuki
 #               <missy at be.to / hmishima at nagasaki-u.ac.jp> 
 # License::     The Ruby licence (Ryby's / GPLv2 dual)
 #
 
-require "#{::File.dirname(__FILE__)}/proteome/db_connection"
+require "#{::File.dirname(__FILE__)}/db_connector"
 require "#{::File.dirname(__FILE__)}/table_class_detector"
 
 module Bio
   module Ucsc
     module Proteome
       extend TableClassDetector
+      include DBConnector
+      DBConnection.database "proteome"
+
       base = "#{::File.dirname(__FILE__)}/proteome"
     end
   end
