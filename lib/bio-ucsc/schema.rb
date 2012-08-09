22c4cec8feea59facf43c5eff349e29b1161457c
Hiroyuki Mishima <missy@be.to>
2012-08-09 10:44:11 +0900

    add sample all.joiner and methods to parse it

--- lib/bio-ucsc/schema.rb
+++ lib/bio-ucsc/schema.rb
@@ -15,21 +15,130 @@ module Bio
       ALL_JOINER_URI =
         "http://genome-source.cse.ucsc.edu/gitweb/" +
         "?p=kent.git;a=blob_plain;f=src/hg/makeDb/schema/all.joiner"
+      EOF = "__EOF__"
 
       class Joiner
+        attr_reader :joiner
 
-      attr_reader :joiner
+        def initialize(text)
+          @joiner = text << "\n" << EOF
+          @variables = nil
+          @identifiers = nil
+        end
 
-      def initialize(text)
-        @joiner = text
+        def self.load(uri=nil)
+          uri ||= ALL_JOINER_URI
+          new(URI(uri).read)
+        end
+
+        def variables
+          if @variables
+            return @variables
+          else
+            parse
+            return @variables
+          end
+        end
+
+        def identifiers
+          if @identifiers
+            return @identifiers
+          else
+            parse
+            return @identifiers
+          end
+        end
+
+        def remove_indent(str)
+          str.chomp.tr("\t", " ").squeeze(" ").sub(/\A /,"")
+        end
+
+        def parse
+          enum = @joiner.lines.reject{|x|x.start_with?('#')}.each
+          here = remove_indent(enum.next)
+          loop do
+            cmd, name, value = here.split(" ",3)
+            case cmd
+            when "set"
+              @variables ||= Variables.new
+              @variables[name] = value.split(",")
+            when "identifier"
+              @identifiers ||= Hash.new
+              @identifiers[name] = Identifier.new
+              if value
+                value.split(" ").each do |attrib|
+                  type, info = attrib.split("=")
+                  @identifiers[name].attributes[type] = info
+                end
+              end
+              here = remove_indent(enum.next)
+              @identifiers[name].comment = here.gsub(/\"/, "")
+              here = remove_indent(enum.next)
+              @identifiers[name].primary_key = here
+              @identifiers[name].fields = Array.new
+              loop do
+                here = remove_indent(enum.next)
+                peek = remove_indent(enum.peek)
+                break if (peek == "" || peek == EOF)
+                @identifiers[name].fields << here
+              end
+            end
+            peek = remove_indent(enum.peek)
+            break if peek == EOF
+            here = remove_indent(enum.next)
+          end
+          self
+        end
+
+      end # class Joiner
+
+      class Variables
+        def initialize
+          @var = Hash.new
+        end
+
+        def []=(key, value)
+          if value.respond_to?(:each)
+            @var[key] = value            
+          else
+            @var[key] = [value]
+          end
+          @var[key]
+        end
+
+        def [](key)
+          return @var[key].map do |v|
+            if v.start_with?("$")
+              self[(v[1..-1])]
+            else
+              v
+            end
+          end
+        end
       end
 
-      def self.load(uri=nil)
-        uri ||= ALL_JOINER_URI
-        new(URI(uri).read)
+      class Identifier
+        attr_accessor :attributes
+        attr_accessor :comment
+        attr_accessor :primary_key
+        attr_accessor :fields
+        
+        def initialize
+          @attributes = Hash.new
+          @comment = nil
+          @primary_key = nil
+          @fields = Hash.new
+        end
       end
 
+      class Dependency
       end
-    end # class Schema
+      
+      class TableType
+      end
+
+      class TablesIgnored
+      end
+    end # module Schema
   end # module Ucsc
 end # module Bio
--- spec/schema_spec.rb
+++ spec/schema_spec.rb
@@ -1,10 +1,92 @@
 require 'bio-ucsc'
+require 'pp'
 
-describe "Bio::Ucsc::Schema::Joiner" do
-  context ".load" do 
-    it "load default all.joiner into @joiner" do
-      o = Bio::Ucsc::Schema::Joiner.load
-      o.joiner.start_with?("# This contains linkage information").should be_true
+describe "Bio::Ucsc::Schema" do
+
+  describe "Variables" do
+    context '#["hg"]=["hg18", "hg19"]' do 
+      it '["hg"] returns a hash "hg" => ["hg18", "hg19"]' do
+        o = Bio::Ucsc::Schema::Variables.new
+        o["hg"] = ["hg18", "hg19"]
+        o["hg"].should == ["hg18", "hg19"]
+      end
+    end
+
+    context '#["hg"]=["hg18", "hg19"] then #["test"]=["hoge","$hg"]' do
+      it '[test] returns ["hoge", ["hg18", "hg19"]]' do
+        o = Bio::Ucsc::Schema::Variables.new
+        o["hg"] = ["hg18", "hg19"]
+        o["test"] = ["hoge","$hg"]
+        o["test"].should == ["hoge", ["hg18", "hg19"]]
+      end
     end
   end
-end
+  
+  describe "Joiner" do
+    describe ".joiner" do
+      context "given default all.joiner at UCSC" do
+        it "returns loaded text" do
+          o = Bio::Ucsc::Schema::Joiner.load
+          o.joiner.start_with?("# This contains linkage information").should be_true
+        end
+      end
+    end
+  
+    describe "#variables" do
+      context "given a string of set statements" do
+        it "returns stored variables" do
+          text = 
+            "# here is a comment line\n" << 
+            "set hg hg16,hg17,hg18,hg19\n"
+          o = Bio::Ucsc::Schema::Joiner.new(text)
+          o.variables["hg"].should == %w(hg16 hg17 hg18 hg19)
+        end
+      end
+    end
+
+    describe "#identifiers" do
+      context "given a string of identifier statements" do
+        it "parses the set statement and stores" do
+          text = 
+            "# here is a comment line\n" << 
+            "set hg hg16,hg17,hg18,hg19\n" <<
+            "set hoge fuga,$hg,muga\n"
+          o = Bio::Ucsc::Schema::Joiner.new(text)
+          o.variables["hoge"].should == ["fuga",["hg16","hg17","hg18","hg19"],"muga"]
+        end
+      end
+    end
+    
+    describe "#variables" do
+      context "given full all.joiner" do
+        it "returns the joiner object" do
+          filename = "samples/src-hg-makeDb-schema-all.joiner.txt"
+          o = Bio::Ucsc::Schema::Joiner.new(File.read(filename))
+          o.variables["cgapSageDbs"].should == %w(hg18 hg19 mm8)
+        end
+      end
+    end
+ 
+    describe "#identifiers" do
+      context "given full all.joiner" do
+        it "returns the joiner object" do
+          filename = "samples/src-hg-makeDb-schema-all.joiner.txt"
+          o = Bio::Ucsc::Schema::Joiner.new(File.read(filename))
+          o.identifiers["goAccessionChopped"].
+            primary_key.should == "go.term.acc chopBefore=GO:"
+        end
+      end
+    end
+
+    # descrive "#find_primary_tables" do
+    #   context "given 'go.term'" do
+    #     it "returns ["go.term.acc", "go.term.acc chopBefore=GO:", ...]" do
+    #       filename = "samples/src-hg-makeDb-schema-all.joiner.txt"
+    #       o = Bio::Ucsc::Schema::Joiner.new(File.read(filename))
+    #       o.find_primary_keys("go.term").first.should == "go.term.acc"
+    #     end
+    #   end
+    # end
+  end # describe "Joiner"
+
+end # describe "Bio::Ucsc::Schema"
