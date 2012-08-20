f869e40cc5b95af478fe8b8ca479188179fe6851
Hiroyuki Mishima <missy@be.to>
2012-08-20 14:47:21 +0900

    update caljac3.rb for multi-db

--- lib/bio-ucsc/caljac3.rb
+++ lib/bio-ucsc/caljac3.rb
@@ -6,7 +6,6 @@
 # License::     The Ruby licence (Ryby's / GPLv2 dual)
 #
 
-require "#{::File.dirname(__FILE__)}/db_connector"
 require "#{::File.dirname(__FILE__)}/table_class_detector"
 
 module Bio
@@ -18,8 +17,47 @@ Chr10 Chr11 Chr12 Chr13 Chr14 Chr15 Chr16 Chr17 Chr18 Chr19
 Chr20 Chr21 Chr22 ChrX ChrY)
 
       extend TableClassDetector
-      include DBConnector
-      DBConnection.database "calJac3"
+      def self.default
+        DBConnection.default
+      end
+
+      def self.connect(param = Hash.new)
+        DBConnection.db_adapter = param[:db_adapter] if param[:db_adapter]
+        DBConnection.db_host = param[:db_host] if param[:db_host]
+        DBConnection.db_username = param[:db_username] if param[:db_username]
+        DBConnection.password = param[:password] if param[:password]
+        DBConnection.connect
+      end
+
+      class DBConnection < ActiveRecord::Base
+        include SafeAttributes
+        self.inheritance_column = 'dummy_not_to_use'
+
+        @@db_adapter  ||= 'mysql'
+        @@db_host     ||= 'genome-mysql.cse.ucsc.edu'
+        @@db_username ||= 'genome'
+        @@db_password ||= ''
+        @@db_name     ||= 'calJac3'
+
+        cattr_accessor :db_adapter, :db_host, :db_username, :db_password
+
+        self.abstract_class = true
+
+        def self.default
+          @@db_adapter  = 'mysql'
+          @@db_host     = 'genome-mysql.cse.ucsc.edu'
+          @@db_username = 'genome'
+          @@db_password = ''
+        end
+
+        def self.connect
+          establish_connection({ :adapter =>  @@db_adapter,
+                                 :host =>     @@db_host,
+                                 :database => @@db_name,
+                                 :username => @@db_username,
+                                 :password => @@db_password, })
+        end
+      end # class DBConnection
 
       base = "#{::File.dirname(__FILE__)}/caljac3"
     end
