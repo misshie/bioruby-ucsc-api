0f5c06f9c32260c6d871aa40fb2e22a68e7a29bc
Hiroyuki Mishima <missy@be.to>
2012-08-21 08:18:35 +0900

    update mondom5.rb for multidb connection

--- lib/bio-ucsc/mondom5.rb
+++ lib/bio-ucsc/mondom5.rb
@@ -6,7 +6,6 @@
 # License::     The Ruby licence (Ryby's / GPLv2 dual)
 #
 
-require "#{::File.dirname(__FILE__)}/db_connector"
 require "#{::File.dirname(__FILE__)}/table_class_detector"
 
 module Bio
@@ -15,8 +14,48 @@ module Bio
       CHROMS = 
 %w(Chr1 Chr2 Chr3 Chr4 Chr5 Chr6 Chr7 Chr8 ChrX ChrM)
       extend TableClassDetector
-      include DBConnector
-      DBConnection.database "monDom5"
+
+      def self.default
+        DBConnection.default
+      end
+
+      def self.connect(param = Hash.new)
+        DBConnection.db_adapter = param[:db_adapter] if param[:db_adapter]
+        DBConnection.db_host = param[:db_host] if param[:db_host]
+        DBConnection.db_username = param[:db_username] if param[:db_username]
+        DBConnection.password = param[:password] if param[:password]
+        DBConnection.connect
+      end
+
+      class DBConnection < ActiveRecord::Base
+        include SafeAttributes
+        self.inheritance_column = 'dummy_not_to_use'
+
+        @@db_adapter  ||= 'mysql'
+        @@db_host     ||= 'genome-mysql.cse.ucsc.edu'
+        @@db_username ||= 'genome'
+        @@db_password ||= ''
+        @@db_name     ||= 'monDom5'
+
+        cattr_accessor :db_adapter, :db_host, :db_username, :db_password
+
+        self.abstract_class = true
+
+        def self.default
+          @@db_adapter  = 'mysql'
+          @@db_host     = 'genome-mysql.cse.ucsc.edu'
+          @@db_username = 'genome'
+          @@db_password = ''
+        end
+
+        def self.connect
+          establish_connection({ :adapter =>  @@db_adapter,
+                                 :host =>     @@db_host,
+                                 :database => @@db_name,
+                                 :username => @@db_username,
+                                 :password => @@db_password, })
+        end
+      end # class DBConnection
 
       base = "#{::File.dirname(__FILE__)}/mondom5"
     end
