5026c8926075dc073b632bdafe18bd1d2c1887d7
Hiroyuki Mishima <missy@be.to>
2012-08-20 14:09:49 +0900

    update aplcal1.rb for multi-db connection

--- lib/bio-ucsc/aplcal1.rb
+++ lib/bio-ucsc/aplcal1.rb
@@ -6,7 +6,6 @@
 # License::     The Ruby licence (Ryby's / GPLv2 dual)
 #
 
-require "#{::File.dirname(__FILE__)}/db_connector"
 require "#{::File.dirname(__FILE__)}/table_class_detector"
 
 module Bio
@@ -15,21 +14,49 @@ module Bio
       CHROMS = %w() # many scaffolds
 
       extend TableClassDetector
-      include DBConnector
-      DBConnection.database "aplCal1"
+      def self.default
+        DBConnection.default
+      end
+
+      def self.connect(param = Hash.new)
+        DBConnection.db_adapter = param[:db_adapter] if param[:db_adapter]
+        DBConnection.db_host = param[:db_host] if param[:db_host]
+        DBConnection.db_username = param[:db_username] if param[:db_username]
+        DBConnection.password = param[:password] if param[:password]
+        DBConnection.connect
+      end
+
+      class DBConnection < ActiveRecord::Base
+        include SafeAttributes
+        self.inheritance_column = 'dummy_not_to_use'
+
+        @@db_adapter  ||= 'mysql'
+        @@db_host     ||= 'genome-mysql.cse.ucsc.edu'
+        @@db_username ||= 'genome'
+        @@db_password ||= ''
+        @@db_name     ||= 'aplCal1'
+
+        cattr_accessor :db_adapter, :db_host, :db_username, :db_password
+
+        self.abstract_class = true
+
+        def self.default
+          @@db_adapter  = 'mysql'
+          @@db_host     = 'genome-mysql.cse.ucsc.edu'
+          @@db_username = 'genome'
+          @@db_password = ''
+        end
+
+        def self.connect
+          establish_connection({ :adapter =>  @@db_adapter,
+                                 :host =>     @@db_host,
+                                 :database => @@db_name,
+                                 :username => @@db_username,
+                                 :password => @@db_password, })
+        end
+      end # class DBConnection
 
       base = "#{::File.dirname(__FILE__)}/aplcal1"
-      # autoload :ChainCaePb1, "#{base}/chaincaepb1"
-      # autoload :ChainCaePb1Link, "#{base}/chaincaepb1link"
-      # autoload :ChainCaeRem2, "#{base}/chaincaerem2"
-      # autoload :ChainCaeRem2Link, "#{base}/chaincaerem2link"
-      # autoload :ChainCb3, "#{base}/chaincb3"
-      # autoload :ChainCb3Link, "#{base}/chaincb3link"
-      # autoload :ChainCe6, "#{base}/chaince6"
-      # autoload :ChainCe6Link, "#{base}/chaince6link"
-      # autoload :Gap, "#{base}/gap"
-      # autoload :Gold, "#{base}/gold"
-      # autoload :Rmsk, "#{base}/rmsk"
     end
   end
 end
