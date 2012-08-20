8ba5fce6ffde57189cf769a9bbdbf8d202baa268
Hiroyuki Mishima <missy@be.to>
2012-08-20 16:01:59 +0900

    update dromoj2.rb for multi-db connection

--- lib/bio-ucsc/dromoj2.rb
+++ lib/bio-ucsc/dromoj2.rb
@@ -6,17 +6,55 @@
 # License::     The Ruby licence (Ryby's / GPLv2 dual)
 #
 
-require "#{::File.dirname(__FILE__)}/db_connector"
 require "#{::File.dirname(__FILE__)}/table_class_detector"
 
 module Bio
   module Ucsc
     module DroMoj2
       CHROMS = %w() # many scaffolds
-      include DBConnector
-      DBConnection.database "droMoj2"
   
       extend TableClassDetector
+      def self.default
+        DBConnection.default
+      end
+
+      def self.connect(param = Hash.new)
+        DBConnection.db_adapter = param[:db_adapter] if param[:db_adapter]
+        DBConnection.db_host = param[:db_host] if param[:db_host]
+        DBConnection.db_username = param[:db_username] if param[:db_username]
+        DBConnection.password = param[:password] if param[:password]
+        DBConnection.connect
+      end
+
+      class DBConnection < ActiveRecord::Base
+        include SafeAttributes
+        self.inheritance_column = 'dummy_not_to_use'
+
+        @@db_adapter  ||= 'mysql'
+        @@db_host     ||= 'genome-mysql.cse.ucsc.edu'
+        @@db_username ||= 'genome'
+        @@db_password ||= ''
+        @@db_name     ||= 'droMoj2'
+
+        cattr_accessor :db_adapter, :db_host, :db_username, :db_password
+
+        self.abstract_class = true
+
+        def self.default
+          @@db_adapter  = 'mysql'
+          @@db_host     = 'genome-mysql.cse.ucsc.edu'
+          @@db_username = 'genome'
+          @@db_password = ''
+        end
+
+        def self.connect
+          establish_connection({ :adapter =>  @@db_adapter,
+                                 :host =>     @@db_host,
+                                 :database => @@db_name,
+                                 :username => @@db_username,
+                                 :password => @@db_password, })
+        end
+      end # class DBConnection
 
       base = "#{::File.dirname(__FILE__)}/dromoj2"
     end
