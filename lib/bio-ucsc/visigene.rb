73edfaa1cd42301a8418a90af12b48721b949bef
Hiroyuki Mishima <missy@be.to>
2012-08-21 11:15:24 +0900

    update visigene.rb for multidb connection

--- lib/bio-ucsc/visigene.rb
+++ lib/bio-ucsc/visigene.rb
@@ -13,8 +13,48 @@ module Bio
   module Ucsc
     module VisiGene
       extend TableClassDetector
-      include DBConnector
-      DBConnection.database "visiGene"
+
+      def self.default
+        DBConnection.default
+      end
+
+      def self.connect(param = Hash.new)
+        DBConnection.db_adapter = param[:db_adapter] if param[:db_adapter]
+        DBConnection.db_host = param[:db_host] if param[:db_host]
+        DBConnection.db_username = param[:db_username] if param[:db_username]
+        DBConnection.password = param[:password] if param[:password]
+        DBConnection.connect
+      end
+
+      class DBConnection < ActiveRecord::Base
+        include SafeAttributes
+        self.inheritance_column = 'dummy_not_to_use'
+
+        @@db_adapter  ||= 'mysql'
+        @@db_host     ||= 'genome-mysql.cse.ucsc.edu'
+        @@db_username ||= 'genome'
+        @@db_password ||= ''
+        @@db_name     ||= 'visiGene'
+
+        cattr_accessor :db_adapter, :db_host, :db_username, :db_password
+
+        self.abstract_class = true
+
+        def self.default
+          @@db_adapter  = 'mysql'
+          @@db_host     = 'genome-mysql.cse.ucsc.edu'
+          @@db_username = 'genome'
+          @@db_password = ''
+        end
+
+        def self.connect
+          establish_connection({ :adapter =>  @@db_adapter,
+                                 :host =>     @@db_host,
+                                 :database => @@db_name,
+                                 :username => @@db_username,
+                                 :password => @@db_password, })
+        end
+      end # class DBConnection
 
       base = "#{::File.dirname(__FILE__)}/visigene"
     end
