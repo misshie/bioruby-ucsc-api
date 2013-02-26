1c999cc0430a85d0b8fc7ddd74977f309a1b7a80
Hiroyuki Mishima <missy@be.to>
2013-02-26 09:57:32 +0900

    supports hg19/altseqpatchsp10

--- spec/hg19/altseqpatchesp10_spec.rb
+++ spec/hg19/altseqpatchesp10_spec.rb
@@ -1,5 +1,5 @@
 require 'bio-ucsc'
-describe "Bio::Ucsc::Hg19::AltSeqPatchesP9" do 
+describe "Bio::Ucsc::Hg19::AltSeqPatchesP10" do 
 
   describe "#find_by_interval" do
     context "given range chr1:1-300,000,000" do
@@ -7,7 +7,7 @@ describe "Bio::Ucsc::Hg19::AltSeqPatchesP9" do
         Bio::Ucsc::Hg19::DBConnection.default
         Bio::Ucsc::Hg19::DBConnection.connect
         i = Bio::GenomicInterval.parse("chr1:1-300,000,000")
-        r = Bio::Ucsc::Hg19::AltSeqPatchesP9.find_by_interval(i)
+        r = Bio::Ucsc::Hg19::AltSeqPatchesP10.find_by_interval(i)
         r.chrom.should == "chr1"
       end
     end
