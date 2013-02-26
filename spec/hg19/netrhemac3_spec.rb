f0a3c4930094c14e206a178cd19c72ed942df58f
Hiroyuki Mishima <missy@be.to>
2013-02-26 10:31:05 +0900

    update hg19/netcanfam3, nethg19patch10, netpatmar2 and netrhemac3

--- spec/hg19/netrhemac3_spec.rb
+++ spec/hg19/netrhemac3_spec.rb
@@ -1,5 +1,5 @@
 require 'bio-ucsc'
-describe "Bio::Ucsc::Hg19::NetPetMar1" do 
+describe "Bio::Ucsc::Hg19::NetRheMac3" do 
 
   describe "#find_by_interval" do
     context 'given range chr1:1-20,000' do
@@ -7,7 +7,7 @@ describe "Bio::Ucsc::Hg19::NetPetMar1" do
         Bio::Ucsc::Hg19::DBConnection.default
         Bio::Ucsc::Hg19::DBConnection.connect
         i = Bio::GenomicInterval.parse("chr1:1-20,000")
-        r = Bio::Ucsc::Hg19::NetPetMar1.find_by_interval(i)
+        r = Bio::Ucsc::Hg19::NetRheMac3.find_by_interval(i)
         r.tName.should == "chr1"
       end
     end
