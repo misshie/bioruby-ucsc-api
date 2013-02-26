fc1e8692fae36676f5bfb826b1a7e344ba5f2dcf
Hiroyuki Mishima <missy@be.to>
2013-02-26 10:20:41 +0900

    support hg19/chainpetmar2 and chainrhamac3

--- spec/hg19/chainrhemac3_spec.rb
+++ spec/hg19/chainrhemac3_spec.rb
@@ -1,13 +1,13 @@
 require 'bio-ucsc'
-describe "Bio::Ucsc::Hg19::ChainPetMar1" do 
+describe "Bio::Ucsc::Hg19::ChainRheMac3" do 
 
   describe "#find_by_interval" do
-    context 'given range chr1:1-20,000' do
+    context 'given range chr1:1-15,000' do
       it 'returns a record with column accessors (r.tName == "chr1")' do
         Bio::Ucsc::Hg19::DBConnection.default
         Bio::Ucsc::Hg19::DBConnection.connect
-        i = Bio::GenomicInterval.parse("chr1:1-20,000")
-        r = Bio::Ucsc::Hg19::ChainPetMar1.find_by_interval(i)
+        i = Bio::GenomicInterval.parse("chr1:1-15,000")
+        r = Bio::Ucsc::Hg19::ChainRheMac3.find_by_interval(i)
         r.tName.should == "chr1"
       end
     end
