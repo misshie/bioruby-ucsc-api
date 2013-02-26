dbf3841fd25802b6909b09f58c0a5ce20831b6ab
Hiroyuki Mishima <missy@be.to>
2013-02-26 10:04:01 +0900

    update hg19/chanincanfam3

--- spec/hg19/chaincanfam3_spec.rb
+++ spec/hg19/chaincanfam3_spec.rb
@@ -1,5 +1,5 @@
 require 'bio-ucsc'
-describe "Bio::Ucsc::Hg19::ChainCanFam2" do 
+describe "Bio::Ucsc::Hg19::ChainCanFam3" do 
 
   describe "#find_by_interval" do
     context 'given range chr1:1-15,000' do
@@ -7,7 +7,7 @@ describe "Bio::Ucsc::Hg19::ChainCanFam2" do
         Bio::Ucsc::Hg19::DBConnection.default
         Bio::Ucsc::Hg19::DBConnection.connect
         i = Bio::GenomicInterval.parse("chr1:1-15,000")
-        r = Bio::Ucsc::Hg19::ChainCanFam2.find_by_interval(i)
+        r = Bio::Ucsc::Hg19::ChainCanFam3.find_by_interval(i)
         r.tName.should == "chr1"
       end
     end
