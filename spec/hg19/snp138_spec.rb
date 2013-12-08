a12e1144ffdea7534ee58c4f2c429d1f77f7c140
Hiroyuki Mishima <missy@be.to>
2013-12-09 08:46:42 +0900

    remove specs for hg19/snp131 and snp132, add specs for hg19/snp138

--- spec/hg19/snp138_spec.rb
+++ spec/hg19/snp138_spec.rb
@@ -1,13 +1,13 @@
 require 'bio-ucsc'
-describe "Bio::Ucsc::Hg19::Snp132" do 
+describe "Bio::Ucsc::Hg19::Snp138" do 
 
   describe "#find_by_interval" do
     context "given range chr1:1-12,000 as a String" do
       it "returns an array of results" do
         Bio::Ucsc::Hg19::DBConnection.default
         Bio::Ucsc::Hg19::DBConnection.connect
-        r = Bio::Ucsc::Hg19::Snp132.find_all_by_interval("chr1:1-12,000")
-        r.should have(67).items
+        r = Bio::Ucsc::Hg19::Snp138.find_all_by_interval("chr1:1-12,000")
+        r.size.should > 1
       end
     end
 
@@ -16,15 +16,15 @@ describe "Bio::Ucsc::Hg19::Snp132" do
         Bio::Ucsc::Hg19::DBConnection.default
         Bio::Ucsc::Hg19::DBConnection.connect
         i = Bio::GenomicInterval.parse("chr1:1-12,000")
-        r = Bio::Ucsc::Hg19::Snp132.find_all_by_interval(i)
-        r.should have(67).items
+        r = Bio::Ucsc::Hg19::Snp138.find_all_by_interval(i)
+        r.size.should > 1
       end
 
       it "returns an array of results with column accessors" do
         Bio::Ucsc::Hg19::DBConnection.default
         Bio::Ucsc::Hg19::DBConnection.connect
         i = Bio::GenomicInterval.parse("chr1:1-12,000")
-        r = Bio::Ucsc::Hg19::Snp132.find_by_interval(i)
+        r = Bio::Ucsc::Hg19::Snp138.find_by_interval(i)
         r.chrom.should == "chr1"
       end
     end
