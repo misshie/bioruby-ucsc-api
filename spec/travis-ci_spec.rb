202631c80f23f1fb1921ee2450f316974d725885
Hiroyuki Mishima <missy@be.to>
2013-12-11 17:24:10 +0900

    update spec/travis-ci_spec.rb

--- spec/travis-ci_spec.rb
+++ spec/travis-ci_spec.rb
@@ -1,10 +1,10 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Bio::Ucsc::Hg19::Snp132" do 
+describe "Bio::Ucsc::Hg19::Snp138" do 
   describe "#find_by_interval" do
     it "returns an array of results with column accessors" do
       Bio::Ucsc::Hg19.connect
-      r = Bio::Ucsc::Hg19::Snp132.find_by_interval("chr1:1-12,000")
+      r = Bio::Ucsc::Hg19::Snp138.find_by_interval("chr1:1-12,000")
       r.chrom.should == "chr1"
     end
   end
